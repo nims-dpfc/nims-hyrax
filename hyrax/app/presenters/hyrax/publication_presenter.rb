2e9b8e0ce5d5cf89990639c183e878307d2623d5
Steven Eardley <steve@eardley.xyz>
2018-12-15 20:02:32 +0000

    added scaffold for publication worktype

---
+++ hyrax/app/presenters/hyrax/publication_presenter.rb
@@ -0,0 +1,6 @@
+# Generated via
+#  `rails generate hyrax:work Publication`
+module Hyrax
+  class PublicationPresenter < Hyrax::WorkShowPresenter
+  end
+end
--- hyrax/config/initializers/hyrax.rb
+++ hyrax/config/initializers/hyrax.rb
@@ -3,6 +3,8 @@ Hyrax.config do |config|
   config.register_curation_concern :work
   # Injected via `rails g hyrax:work Dataset`
   config.register_curation_concern :dataset
+  # Injected via `rails g hyrax:work Publication`
+  config.register_curation_concern :publication
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
