f7c467d96f3c6050146047b10bcbad1e0686ed3d
Steven Eardley <steve@eardley.xyz>
2018-12-15 20:02:32 +0000

    added scaffold for publication worktype

---
+++ hyrax/app/controllers/hyrax/publications_controller.rb
@@ -0,0 +1,14 @@
+# Generated via
+#  `rails generate hyrax:work Publication`
+module Hyrax
+  # Generated controller for Publication
+  class PublicationsController < ApplicationController
+    # Adds Hyrax behaviors to the controller.
+    include Hyrax::WorksControllerBehavior
+    include Hyrax::BreadcrumbsForWorks
+    self.curation_concern_type = ::Publication
+
+    # Use this line if you want to use a custom presenter
+    self.show_presenter = Hyrax::PublicationPresenter
+  end
+end
