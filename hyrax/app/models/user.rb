1c1d12d4e54ae2d76b79037ab9a3ee615980c2eb
Anusha Ranganathan <anusha3@gmail.com>
2018-12-31 03:11:16 +0000

    Removed registrable from devise as users will be created in the console

--- hyrax/app/models/user.rb
+++ hyrax/app/models/user.rb
@@ -17,9 +17,9 @@ class User < ApplicationRecord
   # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :registerable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable,
+         :rememberable, :trackable, :validatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
