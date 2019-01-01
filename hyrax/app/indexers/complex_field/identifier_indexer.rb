1811a4172f7cba87f778bd8df219bcce0e019cac
Anusha Ranganathan <anusha3@gmail.com>
2019-01-01 01:37:55 +0000

    Remove blank terms from label too

--- hyrax/app/indexers/complex_field/identifier_indexer.rb
+++ hyrax/app/indexers/complex_field/identifier_indexer.rb
@@ -10,8 +10,8 @@ module ComplexField
       solr_doc[Solrizer.solr_name('complex_identifier', :symbol)] = object.complex_identifier.map { |i| i.identifier.reject(&:blank?).first }
       solr_doc[Solrizer.solr_name('complex_identifier', :displayable)] = object.complex_identifier.to_json
       object.complex_identifier.each do |i|
-        unless (i.label.first.blank? or i.identifier.first.blank?)
-          fld_name = Solrizer.solr_name("complex_identifier_#{i.label.first.downcase.tr(' ', '_')}", :symbol)
+        unless i.label.blank? || i.identifier.blank?
+          fld_name = Solrizer.solr_name("complex_identifier_#{i.label.reject(&:blank?).first.downcase.tr(' ', '_')}", :symbol)
           solr_doc[fld_name] = [] unless solr_doc.include?(fld_name)
           solr_doc[fld_name] << i.identifier.reject(&:blank?)
           solr_doc[fld_name].flatten!
