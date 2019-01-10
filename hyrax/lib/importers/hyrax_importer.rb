ae5cd3a9b90714448adbe5a1c5aa5337cc652c1d
Anusha Ranganathan <anusha3@gmail.com>
2019-01-10 02:29:49 +0000

    Fixed dataset importer to associate files to works

--- hyrax/lib/importers/hyrax_importer.rb
+++ hyrax/lib/importers/hyrax_importer.rb
@@ -16,7 +16,7 @@ module Importers
     end
 
     def upload_files
-      file_ids = []
+      @file_ids = []
       files.each do |file|
         unless File.file?(file)
           # TODO if there are dirs in the file list, perhaps this should zip them instead of ignoring them
@@ -28,7 +28,7 @@ module Importers
         u.user_id = @current_user.id unless @current_user.nil?
         u.file = ::CarrierWave::SanitizedFile.new(file)
         u.save
-        file_ids << u.id
+        @file_ids << u.id
       end
     end
 
@@ -49,24 +49,23 @@ module Importers
 
       def find_work
         # params[:id] = SecureRandom.uuid unless params[:id].present?
-        return find_work_by_id if work_id
+        return find_work_by_id if @work_id
       end
 
       def find_work_by_id
-        @work_klass.find(work_id)
+        @work_klass.find(@work_id)
         rescue ActiveFedora::ActiveFedoraError
         nil
       end
 
       def update_work
-        raise "Object doesn't exist" unless object
+        raise "Object doesn't exist" unless @object
         work_actor.update(environment(update_attributes))
       end
 
       def create_work
-        attrs = create_attributes
         @object = @work_klass.new
-        work_actor.create(environment(attrs))
+        work_actor.create(environment(create_attributes))
       end
 
       def create_attributes
@@ -92,20 +91,42 @@ module Importers
       # Override if we need to map the attributes from the parser in
       # a way that is compatible with how the factory needs them.
       def transform_attributes
-        @attributes.merge(file_attributes)
         # @attributes.slice(*permitted_attributes).merge(file_attributes)
+        @attributes = @attributes.merge(file_attributes)
+	@attributes
       end
 
       def file_attributes
-        file_ids.present? ? { uploaded_files: file_ids } : {}
+        @file_ids.present? ? { uploaded_files: @file_ids } : {}
       end
 
       def permitted_attributes
-        "::Hyrax::#{klass}Form".constantize.build_permitted_params
+        "::Hyrax::#{@klass}Form".constantize.build_permitted_params
       end
 
       def set_work_klass
         @work_klass = @klass.constantize
+      end
+
+      def visibility_options(visibility)
+        # Filesets inherit visibility for work
+        # TODO: How to set filset attribute different from work
+        possible_options = %w(open authenticated embargo lease restricted)
+        return {} unless possible_options.include? visibility
+        { visibility: visibility }
+      end
+
+      def embargo_options(visibility_during, visibility_after, release_dt)
+        during_options = %w(authenticated restricted)
+        after_options = %w(open authenticated)
+        # Date should be parseable
+        return unless during_options.include? visibility_during
+        return unless after_options.include? visibility_after
+        {
+          visibility_during_embargo: visibility_during,
+          embargo_release_date: release_dt,
+          visibility_after_embargo: visibility_after
+        }
       end
   end
 end
