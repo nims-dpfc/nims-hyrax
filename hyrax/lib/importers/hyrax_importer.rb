cdf595ce0adff672a3e314167e66c41d157e2c1e
Anusha Ranganathan <anusha3@gmail.com>
2019-01-10 09:26:31 +0000

    Modified syntax

--- hyrax/lib/importers/hyrax_importer.rb
+++ hyrax/lib/importers/hyrax_importer.rb
@@ -92,8 +92,7 @@ module Importers
       # a way that is compatible with how the factory needs them.
       def transform_attributes
         # @attributes.slice(*permitted_attributes).merge(file_attributes)
-        @attributes = @attributes.merge(file_attributes)
-	@attributes
+        @attributes.merge!(file_attributes)
       end
 
       def file_attributes
