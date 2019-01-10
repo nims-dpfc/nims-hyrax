7cf88dca8207d4a59f5c812b39d2dc18ec3ea0b0
Steven Eardley <steve@eardley.xyz>
2019-01-10 15:01:43 +0000

    Added fields to presenter and to solr

--- hyrax/app/presenters/hyrax/publication_presenter.rb
+++ hyrax/app/presenters/hyrax/publication_presenter.rb
@@ -2,5 +2,8 @@
 #  `rails generate hyrax:work Publication`
 module Hyrax
   class PublicationPresenter < Hyrax::WorkShowPresenter
+    delegate :alternative_title, :complex_date, :complex_identifier,
+             :complex_person, :complex_rights, :complex_version, :complex_event,
+             :issue, :place, :total_number_of_pages, to: :solr_document
   end
 end
