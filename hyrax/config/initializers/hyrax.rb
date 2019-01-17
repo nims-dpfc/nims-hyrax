0833b1106cfa856c6ede03b027c9a869e9312d3e
Anusha Ranganathan <anusha3@gmail.com>
2019-01-17 10:42:09 +0000

    Created image work type

--- hyrax/config/initializers/hyrax.rb
+++ hyrax/config/initializers/hyrax.rb
@@ -5,6 +5,8 @@ Hyrax.config do |config|
   config.register_curation_concern :dataset
   # Injected via `rails g hyrax:work Publication`
   config.register_curation_concern :publication
+  # Injected via `rails g hyrax:work Image`
+  config.register_curation_concern :image
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
