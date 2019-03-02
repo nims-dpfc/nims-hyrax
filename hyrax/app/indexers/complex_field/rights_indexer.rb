95d93a71ace16ce14b50ae43005faa23d413825b
Anusha Ranganathan <anusha3@gmail.com>
2019-03-02 01:14:49 +0000

    Added index, search and show fields to indexers and called by catalog controller

--- hyrax/app/indexers/complex_field/rights_indexer.rb
+++ hyrax/app/indexers/complex_field/rights_indexer.rb
@@ -10,5 +10,27 @@ module ComplexField
       solr_doc[Solrizer.solr_name('complex_rights', :displayable)] = object.complex_rights.to_json
       solr_doc[Solrizer.solr_name('complex_rights', :facetable)] = object.complex_rights.map { |r| r.rights.reject(&:blank?).first }
     end
+
+    def self.facet_fields
+      # solr fields that will be treated as facets
+      super.tap do |fields|
+        fields << Solrizer.solr_name('complex_rights', :facetable)
+      end
+    end
+
+    def self.search_fields
+      # solr fields that will be used for a search
+      super.tap do |fields|
+        fields << Solrizer.solr_name('complex_rights', :facetable)
+      end
+    end
+
+    def self.show_fields
+      # solr fields that will be used to display results on the record page
+      super.tap do |fields|
+        fields << Solrizer.solr_name('complex_rights', :displayable)
+      end
+    end
+
   end
 end
