4c550121fcd79ba68d83982479c08e4ce3bc0734
Anusha Ranganathan <anusha3@gmail.com>
2019-03-10 23:05:30 +0000

    Defined facet, show and index fields for images

--- hyrax/app/indexers/image_indexer.rb
+++ hyrax/app/indexers/image_indexer.rb
@@ -17,4 +17,25 @@ class ImageIndexer < Hyrax::WorkIndexer
   include ComplexField::VersionIndexer
   include ComplexField::CustomPropertyIndexer
   include ComplexField::RelationIndexer
+
+  def self.facet_fields
+    super.tap do |fields|
+      fields << Solrizer.solr_name('instrument', :facetable)
+      fields << Solrizer.solr_name('specimen_set', :facetable)
+    end
+  end
+
+  def self.search_fields
+    super.tap do |fields|
+      fields << Solrizer.solr_name('instrument', :stored_searchable)
+      fields << Solrizer.solr_name('specimen_set', :stored_searchable)
+    end
+  end
+
+  def self.show_fields
+    super.tap do |fields|
+      fields << Solrizer.solr_name('instrument', :stored_searchable)
+      fields << Solrizer.solr_name('specimen_set', :stored_searchable)
+    end
+  end
 end
