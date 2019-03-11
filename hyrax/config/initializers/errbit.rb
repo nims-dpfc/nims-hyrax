3cbcc0d718e0b19f10d9fa2bb24ecea4d069a05a
Anusha Ranganathan <anusha3@gmail.com>
2019-03-11 21:09:17 +0000

    Added errbit env variables to the template

--- hyrax/config/initializers/errbit.rb
+++ hyrax/config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.host = ENV['AIRBRAKE_HOST']
-  config.project_id = ENV['PROJECT_ID']
-  config.project_key = ENV['PROJECT_KEY']
+  config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
 
   config.environment = Rails.env
   config.ignore_environments = %w(development test)
