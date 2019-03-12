a7811baffbb0e847913cf48d6012bb25cd1db063
Anusha Ranganathan <anusha3@gmail.com>
2019-03-12 11:53:19 +0000

    Initializingf errbit only if the environment variables are configured

--- hyrax/config/initializers/errbit.rb
+++ hyrax/config/initializers/errbit.rb
@@ -1,8 +1,12 @@
-Airbrake.configure do |config|
-  config.host = ENV['AIRBRAKE_HOST']
-  config.project_id = ENV['AIRBRAKE_PROJECT_ID']
-  config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
-
-  config.environment = Rails.env
-  config.ignore_environments = %w(development test)
+if ENV.fetch('AIRBRAKE_HOST', nil).present? and
+  ENV.fetch('AIRBRAKE_PROJECT_ID', nil).present? and
+  ENV.fetch('AIRBRAKE_PROJECT_KEY', nil).present?
+  Airbrake.configure do |config|
+    config.host = ENV['AIRBRAKE_HOST']
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
+    # setting for the rails app
+    config.environment = Rails.env
+    config.ignore_environments = %w(development test)
+  end
 end
