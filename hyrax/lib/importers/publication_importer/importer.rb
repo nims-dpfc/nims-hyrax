79c3766f204b6d9a8835992ad76bc9a145a1b469
Anusha <anusha3@gmail.com>
2019-03-17 00:39:11 +0000

    Add rotation for log file

--- hyrax/lib/importers/publication_importer/importer.rb
+++ hyrax/lib/importers/publication_importer/importer.rb
@@ -14,10 +14,10 @@ module Importers
         @import_dir = import_dir
         @metadata_file = metadata_file
         @debug = debug
-        @log_file = log_file
-        @log_file = "data/#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}_import_publication_log.csv" if log_file.blank?
+        @log_file = nil
         @add_to_collection = add_to_collection
         @collection = nil
+        @count = 0
       end
 
       def perform_create
@@ -92,6 +92,7 @@ module Importers
       end
 
       def log_progress(metadata_file, id, collection, files, missing_files, attributes, error)
+        log_rotate
         write_headers = true
         write_headers = false if File.file?(@log_file)
         csv_file = CSV.open(@log_file, "ab")
@@ -118,6 +119,14 @@ module Importers
           JSON.pretty_generate(error)
         ]
         csv_file.close
+      end
+
+      def log_rotate
+        get_log_file if @log_file.blank or (@count % 1000 == 0)
+      end
+
+      def get_log_file
+        @log_file = "data/#{Time.now.strftime('%Y-%m-%d-%H-%M-%S')}_import_publication_log.csv"
       end
     end
   end
