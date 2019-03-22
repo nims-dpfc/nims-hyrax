1c7aeaa9f2051821191e37712f13ec3fce363149
Steven Eardley <steve@eardley.xyz>
2019-03-22 01:07:28 +0000

    Initial generation of blacklight OAI

--- hyrax/app/models/solr_document.rb
+++ hyrax/app/models/solr_document.rb
@@ -2,6 +2,7 @@
 class SolrDocument
   include Blacklight::Solr::Document
   include Blacklight::Gallery::OpenseadragonSolrDocument
+  include BlacklightOaiProvider::SolrDocument
 
   # Adds Hyrax behaviors to the SolrDocument.
   include Hyrax::SolrDocumentBehavior
