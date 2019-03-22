ab853c2286269ca4afb77d1eb559d0c9ffa6742c
Steven Eardley <steve@eardley.xyz>
2019-03-22 01:07:28 +0000

    Initial generation of blacklight OAI

--- hyrax/app/controllers/catalog_controller.rb
+++ hyrax/app/controllers/catalog_controller.rb
@@ -1,6 +1,7 @@
 class CatalogController < ApplicationController
   include Hydra::Catalog
   include Hydra::Controller::ControllerBehavior
+  include BlacklightOaiProvider::Controller
 
   # This filter applies the hydra access controls
   before_action :enforce_show_permissions, only: :show
