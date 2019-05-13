dc594f3f496269262c8825bfab3e7aa87dfa6a8a
Steven Eardley <steve@eardley.xyz>
2019-05-13 19:14:37 +0100

    Updates pertaining to the older version of blacklight oai

--- hyrax/app/models/solr_document.rb
+++ hyrax/app/models/solr_document.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class SolrDocument
   include Blacklight::Solr::Document
-  include BlacklightOaiProvider::SolrDocument
+  include BlacklightOaiProvider::SolrDocumentBehavior
   include Blacklight::Gallery::OpenseadragonSolrDocument
 
   # Adds Hyrax behaviors to the SolrDocument.
