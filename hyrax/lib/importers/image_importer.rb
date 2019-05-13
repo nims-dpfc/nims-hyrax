e92516d61518f22b0cf8dc086e9ef30cf7c8e916
Anusha Ranganathan <anusha3@gmail.com>
2019-05-13 14:02:46 +0100

    Fixed require statements for publication and image importer

--- hyrax/lib/importers/image_importer.rb
+++ hyrax/lib/importers/image_importer.rb
@@ -2,9 +2,9 @@ require 'importers/hyrax_importer'
 
 module Importers
   module ImageImporter
-    require collections
-    require profiles
-    require parse_xml
-    require importer
+    require 'importers/image_importer/collections'
+    require 'importers/image_importer/profiles'
+    require 'importers/image_importer/parse_xml'
+    require 'importers/image_importer/importer'
   end
 end
