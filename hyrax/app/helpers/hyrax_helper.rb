ce1e8c551295b0e75dead3eaa9f04126b18fe1f9
Anusha Ranganathan <anusha3@gmail.com>
2019-05-22 14:09:12 +0100

    Set english as the only available translation

--- hyrax/app/helpers/hyrax_helper.rb
+++ hyrax/app/helpers/hyrax_helper.rb
@@ -2,4 +2,16 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+
+  def available_translations
+    {
+      # 'de' => 'Deutsch',
+      'en' => 'English',
+      # 'es' => 'Español',
+      # 'fr' => 'Français',
+      # 'it' => 'Italiano',
+      # 'pt-BR' => 'Português do Brasil',
+      # 'zh' => '中文'
+    }
+  end
 end
