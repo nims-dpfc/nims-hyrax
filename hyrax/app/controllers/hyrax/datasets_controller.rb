2889c93e3c6b36eed98398b4fadf976bf4909f43
Anusha Ranganathan <anusha3@gmail.com>
2019-05-24 02:14:44 +0100

    Added a cleanup methos to remove blanks from the nested metadata for datasets

--- hyrax/app/controllers/hyrax/datasets_controller.rb
+++ hyrax/app/controllers/hyrax/datasets_controller.rb
@@ -10,5 +10,74 @@ module Hyrax
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::DatasetPresenter
+
+    def create
+      safe_dataset_params = params['dataset'].permit(::Hyrax::DatasetForm.build_permitted_params)
+      params['dataset'] = cleanup_params(safe_dataset_params.to_h)
+      super
+    end
+
+    def update
+      safe_dataset_params = params['dataset'].permit(::Hyrax::DatasetForm.build_permitted_params)
+      params['dataset'] = cleanup_params(safe_dataset_params.to_h)
+      super
+    end
+
+    private
+
+    def is_complex?(attribute_name)
+      complex_prefixes = [
+        'complex',
+        'instrument_function',
+        'manufacturer',
+        'managing_organization',
+        'supplier',
+        'manufacturer',
+        'custom_property'
+      ]
+      complex_prefixes.each { |prefix| return true if attribute_name.to_s.start_with? prefix }
+      false
+    end
+
+    def hash_is_blank?(hash_param)
+      other_keys = hash_param.keys - ['id', '_destroy']
+      return false unless other_keys.blank?
+      if hash_param.fetch('id', nil) and hash_param.fetch('_destroy', nil)
+        return false if hash_param['_destroy'] == "true"
+      end
+      true
+    end
+
+    def cleanup_params(attribute)
+      if attribute.kind_of? Hash and attribute.keys.all? {|k| k !~ /\D/ }
+        # removes hashes with integer keys
+        attribute = cleanup_params(attribute.values)
+      elsif attribute.kind_of? Hash
+        new_attribute = {}
+        attribute.each do |k, v|
+          v = cleanup_params(v) if is_complex?(k)
+          v = cleanup_params(v) if v.kind_of? Array
+          unless v.blank?
+            new_attribute[k] = v
+          end
+        end
+        unless hash_is_blank?(new_attribute)
+          new_attribute.compact
+        else
+          nil
+        end
+      elsif attribute.kind_of? Array
+        new_attr = []
+        attribute.reject(&:blank?).each do |v|
+          v = cleanup_params(v) if v.kind_of? Hash
+          new_attr << v unless v.blank?
+        end
+        new_attr.reject(&:blank?)
+      elsif attribute.blank?
+        nil
+      else
+        attribute
+      end
+    end
   end
 end
