115f4b92b6ec8eff28b969c7ba49fd7a9614b74a
Anusha Ranganathan <anusha3@gmail.com>
2019-05-30 02:53:52 +0100

    Clean up complex params for publications

--- hyrax/app/controllers/hyrax/publications_controller.rb
+++ hyrax/app/controllers/hyrax/publications_controller.rb
@@ -6,9 +6,23 @@ module Hyrax
     # Adds Hyrax behaviors to the controller.
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
+    include ::ComplexFieldsBehavior
     self.curation_concern_type = ::Publication
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::PublicationPresenter
+
+    def create
+      safe_params = params['publication'].permit(::Hyrax::PublicationForm.build_permitted_params)
+      params['publication'] = cleanup_params(safe_params.to_h)
+      super
+    end
+
+    def update
+      safe_params = params['publication'].permit(::Hyrax::PublicationForm.build_permitted_params)
+      params['publication'] = cleanup_params(safe_params.to_h)
+      super
+    end
+
   end
 end
