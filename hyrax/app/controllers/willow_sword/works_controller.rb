94e379691240cfbe666a20ecfde4326ad32631e4
Anusha Ranganathan <anusha3@gmail.com>
2019-06-13 00:34:22 +0100

    New xml crosswalk for mdr and added method to unpackzip files in payload

---
+++ hyrax/app/controllers/willow_sword/works_controller.rb
@@ -0,0 +1,27 @@
+require_dependency "willow_sword/application_controller"
+
+module WillowSword
+  class WorksController < ApplicationController
+    include WillowSword::WorksBehavior
+    include WillowSword::ProcessRequest
+    include ::Integrator::Hyrax::WorksBehavior
+
+    def bag_request
+      # The data is processed as a bag
+      contents_path = File.join(@dir, 'contents')
+      bag_path = File.join(@dir, 'bag')
+      unpack_zip_files(contents_path)
+      # validate or create bag
+      bag = WillowSword::BagPackage.new(contents_path, bag_path)
+      @metadata_file = File.join(bag.package.data_dir, WillowSword.config.metadata_filename)
+      @files = bag.package.bag_files - [@metadata_file]
+    end
+
+    def unpack_zip_files(contents_path)
+      zip_files = Dir.glob(File.join(contents_path, '*.zip'))
+      zip_files.each do |zip_file|
+        WillowSword::ZipPackage.new(zip_file, contents_path).unzip_file
+      end
+    end
+  end
+end
