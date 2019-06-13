fe90b8ce47600f840afe65b9fc0009d658c30600
Anusha Ranganathan <anusha3@gmail.com>
2019-06-13 01:13:58 +0100

    Added before_action and set_work_klass

--- hyrax/app/controllers/willow_sword/works_controller.rb
+++ hyrax/app/controllers/willow_sword/works_controller.rb
@@ -2,6 +2,9 @@ require_dependency "willow_sword/application_controller"
 
 module WillowSword
   class WorksController < ApplicationController
+    before_action :set_work_klass
+    attr_reader :object, :current_user
+
     include WillowSword::WorksBehavior
     include WillowSword::ProcessRequest
     include ::Integrator::Hyrax::WorksBehavior
