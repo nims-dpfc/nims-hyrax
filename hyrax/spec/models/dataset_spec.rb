78621643dc351ec6d4c29dceb582dbae79e5cd80
Asahiko Matsuda <MATSUDA.Asahiko@nims.go.jp>
2019-06-28 15:04:22 +0900

    #141: change some labels

--- hyrax/spec/models/dataset_spec.rb
+++ hyrax/spec/models/dataset_spec.rb
@@ -967,7 +967,7 @@ RSpec.describe Dataset do
   end
 
   describe 'custom_property' do
-    it 'creates a custom property active triple resource with all the attributes' do
+    it 'creates a custom property (additional metadata) active triple resource with all the attributes' do
       @obj = build(:dataset,
         custom_property_attributes: [{
           label: 'Full name',
@@ -980,7 +980,7 @@ RSpec.describe Dataset do
       expect(@obj.custom_property.first.description).to eq ['My full name is ...']
     end
 
-    it 'rejects a custom property active triple with no label' do
+    it 'rejects a custom property (additional metadata) active triple with no label' do
       @obj = build(:dataset,
         custom_property_attributes: [{
           description: 'Local date'
@@ -989,7 +989,7 @@ RSpec.describe Dataset do
       expect(@obj.custom_property).to be_empty
     end
 
-    it 'rejects a custom property active triple with no description' do
+    it 'rejects a custom property (additional metadata) active triple with no description' do
       @obj = build(:dataset,
         custom_property_attributes: [{
           label: 'Local date'
