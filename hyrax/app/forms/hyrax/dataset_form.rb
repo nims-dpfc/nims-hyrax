6acdda7772259632f19b342d3de63aacd08534c0
Kosuke Tanabe <nabeta@fastmail.fm>
2019-08-01 21:09:17 +0900

    add application_number to Dataset and Publication

--- hyrax/app/forms/hyrax/dataset_form.rb
+++ hyrax/app/forms/hyrax/dataset_form.rb
@@ -26,7 +26,8 @@ module Hyrax
       :complex_identifier,
       :data_origin, :complex_instrument, :origin_system_provenance,
       :properties_addressed, :complex_relation, :specimen_set,
-      :complex_specimen_type, :synthesis_and_processing, :custom_property
+      :complex_specimen_type, :synthesis_and_processing, :custom_property,
+      :application_number
     ]
 
     self.required_fields -= [
