5bf14606b8c770de3db79b1da3ff978d36155855
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2019-08-09 16:51:27 +0900

    create User account when LDAP user signs in for the first time

--- hyrax/app/controllers/omniauth_callbacks_controller.rb
+++ hyrax/app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def orcid
-    Rails.logger.info "OmniauthCallbacksController#orcid: request.env['omniauth.auth']: #{request.env['omniauth.auth']}"
-    # had to create the `from_omniauth(auth_hash)` class method on our User model
+    Rails.logger.debug "OmniauthCallbacksController#orcid: request.env['omniauth.auth']: #{request.env['omniauth.auth']}"
     user = User.from_omniauth(request.env["omniauth.auth"])
     set_flash_message :notice, :success, kind: "ORCID"
     sign_in_and_redirect user
