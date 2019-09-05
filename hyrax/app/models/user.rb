75f37709b1ba90362ad93991016cacbe593bf875
Steven Eardley <steve@eardley.xyz>
2019-09-05 09:37:03 +0100

    Removed :recoverable for users to disable password reminder https://github.com/antleaf/nims-mdr-development/issues/177

--- hyrax/app/models/user.rb
+++ hyrax/app/models/user.rb
@@ -18,7 +18,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :registerable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise ENV.fetch('MDR_DEVISE_AUTH_MODULE', 'database_authenticatable').to_sym,
-         :recoverable,
          :rememberable, :trackable, :validatable, :lockable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
