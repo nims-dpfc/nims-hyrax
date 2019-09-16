4a530d2c910332017f4bc7b64249ff1ba56bcc99
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-09-16 18:00:58 +0100

    #192 - add simplecov code coverage gem

--- hyrax/spec/spec_helper.rb
+++ hyrax/spec/spec_helper.rb
@@ -1,3 +1,14 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  # additional code coverage groups for Hyrax
+  add_group 'Actors', 'app/actors'
+  add_group 'Forms', 'app/forms'
+  add_group 'Indexers', 'app/indexers'
+  add_group 'Inputs', 'app/inputs'
+  add_group 'Presenters', 'app/presenters'
+  add_group 'Renderers', 'app/renderers'
+  add_group 'Services', 'app/services'
+end
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
