dd99ff00f748a2cca597000746d62c6d7adb70de
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-10-02 16:42:22 +0100

    Update qa_spec.rb

--- hyrax/spec/models/qa_spec.rb
+++ hyrax/spec/models/qa_spec.rb
@@ -1,8 +1,10 @@
-require 'rails_helper'
+# NB: using spec_helper instead of rails_helper here to avoid the QA gem from overriding the module in this test
+require 'spec_helper'
+require_relative '../../app/models/qa'
 
 RSpec.describe Qa do
   describe 'table_name_prefix' do
     subject { described_class.table_name_prefix }
-    it {is_expected.to eql 'qa_'}
+    it { is_expected.to eql 'qa_'}
   end
 end
