f6a0c62809cc496e5f24dabe37eb24da63307182
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-10-14 11:03:59 +0100

    form specs

--- hyrax/spec/forms/hyrax/dataset_form_spec.rb
+++ hyrax/spec/forms/hyrax/dataset_form_spec.rb
@@ -1,9 +1,56 @@
-# Generated via
-#  `rails generate hyrax:work Dataset`
 require 'rails_helper'
 
 RSpec.describe Hyrax::DatasetForm do
-  it "has tests" do
-    skip "Add your tests here"
+  it { expect(described_class).to be < Hyrax::Forms::WorkForm }
+
+  describe 'instance methods' do
+    let(:model) { build(:dataset) }
+    let(:ability) { Ability.new(create(:user)) }
+    let(:controller) { nil } # doesn't require a value for these tests
+    let(:form) { described_class.new(model, ability, controller) }
+
+    describe '#metadata_tab_terms' do
+      subject { form.metadata_tab_terms }
+      it { is_expected.to include(:supervisor_approval, :title, :alternative_title, :data_origin, :description,
+                                  :keyword, :specimen_set, :complex_person, :complex_identifier, :complex_date,
+                                  :complex_rights, :complex_version, :complex_relation, :custom_property) }
+    end
+
+    describe '#method_tab_terms' do
+      subject { form.method_tab_terms }
+      it { is_expected.to include(:characterization_methods, :computational_methods, :properties_addressed, :synthesis_and_processing) }
+    end
+
+    describe '#instrument_tab_terms' do
+      subject { form.instrument_tab_terms }
+      it { is_expected.to include(:complex_instrument) }
+    end
+
+    describe '#specimen_tab_terms' do
+      subject { form.specimen_tab_terms }
+      it { is_expected.to include(:complex_specimen_type) }
+    end
+  end
+
+  describe '#build_permitted_params' do
+    subject { described_class.build_permitted_params }
+
+    it { is_expected.to include(:member_of_collection_ids, :find_child_work) }
+
+    context 'permitted params' do
+      it do
+        expect(described_class).to receive(:permitted_date_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_identifier_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_instrument_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_person_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_organization_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_relation_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_rights_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_specimen_type_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_version_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_custom_property_params).at_least(:once).and_call_original
+        subject
+      end
+    end
   end
 end
