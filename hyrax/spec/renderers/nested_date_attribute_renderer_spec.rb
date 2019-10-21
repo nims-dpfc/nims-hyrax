39685d5fa8220ca65a1295bbb948a631f364ecd7
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-10-21 12:48:24 +0100

    add first renderer spec

---
+++ hyrax/spec/renderers/nested_date_attribute_renderer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe NestedDateAttributeRenderer do
+  let(:html) { described_class.new('Date', nested_value.to_json).render }
+  subject { Capybara.string(html) }
+
+  context 'valid date' do
+    let(:nested_value) { build(:dataset, :with_complex_date).complex_date.first }
+    it 'generates the correct fields' do
+      is_expected.to have_css('div.row label', text: 'Published')
+      is_expected.to have_css('div.row', text: '28/10/1978')
+    end
+  end
+
+  context 'invalid date' do
+    let(:nested_value) { { date: ["Foo"], description: ["Bar"] } }
+    it 'generates the correct fields' do
+      is_expected.to have_css('div.row label', text: 'Bar')
+      is_expected.to have_css('div.row', text: 'Foo')
+    end
+  end
+end
