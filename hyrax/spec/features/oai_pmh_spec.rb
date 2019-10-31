b5cb97175b62b706909c9e9ea9acd84a8f8d5074
Kosuke Tanabe <nabeta@fastmail.fm>
2019-10-31 12:12:39 +0900

    fix route to oai_provider, use email as depositor

--- hyrax/spec/features/oai_pmh_spec.rb
+++ hyrax/spec/features/oai_pmh_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe "OAI PMH Support", type: :feature do
   let(:user) { create(:user) }
-  let(:work) { create(:work, depositor: user) }
+  let(:work) { create(:work, depositor: user.email) }
   let(:identifier) { work.id }
 
   before do
@@ -12,24 +12,24 @@ RSpec.describe "OAI PMH Support", type: :feature do
 
   context 'oai interface with works present' do
     it 'lists metadata prefixess' do
-      visit oai_catalog_path(verb: 'ListMetadataFormats')
+      visit oai_provider_catalog_path(verb: 'ListMetadataFormats')
       expect(page).to have_content('oai_dc')
     end
 
     it 'retrieves a list of records' do
-      visit oai_catalog_path(verb: 'ListRecords', metadataPrefix: 'oai_dc')
+      visit oai_provider_catalog_path(verb: 'ListRecords', metadataPrefix: 'oai_dc')
       expect(page).to have_content("oai:ngdrdemo:#{identifier}")
       expect(page).to have_content(work.title.first)
     end
 
     it 'retrieves a single record' do
-      visit oai_catalog_path(verb: 'GetRecord', metadataPrefix: 'oai_dc', identifier: identifier)
+      visit oai_provider_catalog_path(verb: 'GetRecord', metadataPrefix: 'oai_dc', identifier: identifier)
       expect(page).to have_content("oai:ngdrdemo:#{identifier}")
       expect(page).to have_content(work.title.first)
     end
 
     it 'retrieves a list of identifiers' do
-      visit oai_catalog_path(verb: 'ListIdentifiers', metadataPrefix: 'oai_dc')
+      visit oai_provider_catalog_path(verb: 'ListIdentifiers', metadataPrefix: 'oai_dc')
       expect(page).to have_content("oai:ngdrdemo:#{identifier}")
       expect(page).not_to have_content(work.title.first)
     end
