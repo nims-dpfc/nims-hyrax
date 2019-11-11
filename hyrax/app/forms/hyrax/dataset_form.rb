976811fa72362411d5b1f95d34c1c37062fbc76a
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-11-11 18:25:12 +0000

    wip - now authenticating with local cas server

--- hyrax/app/forms/hyrax/dataset_form.rb
+++ hyrax/app/forms/hyrax/dataset_form.rb
@@ -26,8 +26,7 @@ module Hyrax
       :complex_identifier,
       :data_origin, :complex_instrument, :origin_system_provenance,
       :properties_addressed, :complex_relation, :specimen_set,
-      :complex_specimen_type, :synthesis_and_processing, :custom_property,
-      :application_number
+      :complex_specimen_type, :synthesis_and_processing, :custom_property
     ]
 
     self.required_fields -= [
