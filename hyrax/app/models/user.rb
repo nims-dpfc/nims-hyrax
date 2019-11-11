70cbe6a7eaca7727677a1813049069c9468cbe1d
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-11-11 18:25:12 +0000

    wip - now authenticating with local cas server

--- hyrax/app/models/user.rb
+++ hyrax/app/models/user.rb
@@ -18,7 +18,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :registerable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise ENV.fetch('MDR_DEVISE_AUTH_MODULE', 'database_authenticatable').to_sym,
-         :rememberable, :trackable, :validatable, :lockable
+         :rememberable, :trackable, :lockable
+         # NB: the :validatable module is not compatible with CAS authentication
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
@@ -35,5 +36,23 @@ class User < ApplicationRecord
   def ldap_before_save
     self.email = Devise::LDAP::Adapter.get_ldap_param(username, "mail").first
     self.password = Devise.friendly_token[0, 20]
+  end
+
+  def cas_extra_attributes=(extra_attributes)
+    extra_attributes.each do |name, value|
+      case name.to_sym
+      # TODO: change these mappings to match NIMS CAS schema
+      when :mail
+        self.email = value
+      when :eduPersonNickname
+        self.display_name = value
+      when :cn
+        self.email = value
+      # when :fullname
+      #   self.fullname = value
+      # when :email
+      #   self.email = value
+      end
+    end
   end
 end
