32a166f80f7540955b8a38bea87b11679fc32d1f
Julie Allinson <julie@notch8.com>
2019-11-29 11:43:11 +0000

    changes to airbrake config, default in .env.template to db auth

--- hyrax/config/initializers/errbit.rb
+++ hyrax/config/initializers/errbit.rb
@@ -1,12 +1,14 @@
-if ENV.fetch('AIRBRAKE_HOST', nil).present? and
-  ENV.fetch('AIRBRAKE_PROJECT_ID', nil).present? and
-  ENV.fetch('AIRBRAKE_PROJECT_KEY', nil).present?
-  Airbrake.configure do |config|
-    config.host = ENV['AIRBRAKE_HOST']
-    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
-    config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
-    # setting for the rails app
-    config.environment = Rails.env
-    config.ignore_environments = %w(development test)
-  end
+# frozen_string_literal: true
+
+Airbrake.configure do |config|
+  # setting for the rails app
+  config.environment = Rails.env
+  config.ignore_environments = %w[development test]
+  # ignore production if the environment variables aren't set
+  config.ignore_environments << 'production' if ENV.fetch('AIRBRAKE_HOST', nil).blank? &&
+                                                ENV.fetch('AIRBRAKE_PROJECT_ID', nil).blank? &&
+                                                ENV.fetch('AIRBRAKE_PROJECT_KEY', nil).blank?
+  config.host = ENV['AIRBRAKE_HOST']
+  config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
 end
