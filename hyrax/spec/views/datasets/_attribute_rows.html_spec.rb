1b7c84b87ff97a860b70e1194d9c61c8e0fc3c88
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-12-03 16:23:42 +0000

    new view spec for dataset metadata attributes

---
+++ hyrax/spec/views/datasets/_attribute_rows.html_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.describe 'hyrax/datasets/_attribute_rows' do
+  let(:partial) { 'hyrax/datasets/attribute_rows' }
+  let(:dataset) { build(:dataset, :with_alternative_title, :with_complex_person, :with_keyword, :with_subject, :with_language,
+                        :with_publisher, :with_complex_date, :with_complex_identifier, :with_complex_rights,
+                        :with_complex_version, :with_resource_type, :with_complex_relation, :with_source) }
+  let(:presenter) { Hyrax::DatasetPresenter.new(SolrDocument.new(dataset.to_solr), Ability.new(user)) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(user)
+    login_as user if user.present?
+    render partial: partial, locals: { presenter: presenter }
+  end
+
+  # NB: the visibility of individual metadata components is set in app/models/ability.rb
+  # This test confirms the current expected behaviour (which is most metadata is visible)
+
+  context 'unauthenticated user' do
+    let(:user) { nil }
+    it 'shows the correct metadata' do
+      expect(rendered).to have_content('Dataset Alternative Title')
+      expect(rendered).to have_content('Anamika')
+      expect(rendered).to have_content('University')
+      expect(rendered).to have_content('Keyword123')
+      expect(rendered).to have_content('Subject123')
+      expect(rendered).to have_content('Faroese')
+      expect(rendered).to have_content('Publisher123')
+      expect(rendered).to have_content('1978-10-28')
+      expect(rendered).to have_content('10.0.1111')
+      expect(rendered).to have_content('http://creativecommons.org/publicdomain/zero/1.0/')
+      expect(rendered).to have_content('Creating the first version')
+      expect(rendered).to have_content('ResourceType123')
+      expect(rendered).to have_content('A relation label')
+      expect(rendered).to have_content('Source123')
+    end
+  end
+
+  context 'authenticated NIMS Researcher' do
+    let(:user) { create(:user, :nims_researcher) }
+    it 'shows the correct metadata' do
+      expect(rendered).to have_content('Dataset Alternative Title')
+      expect(rendered).to have_content('Anamika')
+      expect(rendered).to have_content('University')
+      expect(rendered).to have_content('Keyword123')
+      expect(rendered).to have_content('Subject123')
+      expect(rendered).to have_content('Faroese')
+      expect(rendered).to have_content('Publisher123')
+      expect(rendered).to have_content('1978-10-28')
+      expect(rendered).to have_content('10.0.1111')
+      expect(rendered).to have_content('http://creativecommons.org/publicdomain/zero/1.0/')
+      expect(rendered).to have_content('Creating the first version')
+      expect(rendered).to have_content('ResourceType123')
+      expect(rendered).to have_content('A relation label')
+      expect(rendered).to have_content('Source123')
+    end
+  end
+end
