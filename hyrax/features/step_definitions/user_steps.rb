2c6cd4e5839ede4e1f5ab33018e3fcaa3c0da87d
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-12-04 16:46:10 +0000

    ensure only nims researchers can create content

--- hyrax/features/step_definitions/user_steps.rb
+++ hyrax/features/step_definitions/user_steps.rb
@@ -1,6 +1,6 @@
 include Warden::Test::Helpers
 
-Given(/^an? (general|admin) user$/) do |user_type|
+Given(/^an? (guest|nims_other|nims_researcher|admin) user$/) do |user_type|
   @user = FactoryBot.create(:user, user_type.to_sym)
 end
 
@@ -8,7 +8,7 @@ Given(/^I am logged in$/) do
   login_as @user
 end
 
-Given(/^I am logged in as an? (general|admin) user$/) do |user_type|
+Given(/^I am logged in as an? (guest|nims_other|nims_researcher|admin) user$/) do |user_type|
   step "a #{user_type} user"
   step 'I am logged in'
 end
