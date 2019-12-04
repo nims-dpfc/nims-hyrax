71a2aeaeab4e383161f065fd5c1c992295cfab8f
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-12-04 10:32:04 +0000

    image view tests

--- hyrax/spec/views/hyrax/images/_attribute_rows.html_spec.rb
+++ hyrax/spec/views/hyrax/images/_attribute_rows.html_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.describe 'hyrax/publications/_attribute_rows' do
-  let(:partial) { 'hyrax/publications/attribute_rows' }
-  let(:publication) { build(:publication, :with_alternative_title, :with_people, :with_keyword, :with_subject, :with_language,
-                        :with_publisher, :with_complex_date, :with_complex_identifier, :with_rights_statement, :with_complex_rights,
-                        :with_complex_version, :with_resource_type, :with_source, :with_issue, :with_complex_source, :with_complex_event,
-                        :with_place, :with_table_of_contents, :with_number_of_pages) }
-  let(:presenter) { Hyrax::PublicationPresenter.new(SolrDocument.new(publication.to_solr), Ability.new(user)) }
+RSpec.describe 'hyrax/images/_attribute_rows' do
+  let(:partial) { 'hyrax/images/attribute_rows' }
+  let(:image) { build(:image, :with_alternative_title, :with_subject, :with_publisher, :with_language,
+                            :with_keyword, :with_resource_type, :with_rights_statement, :with_complex_date,
+                            :with_complex_identifier, :with_complex_person, :with_complex_rights,
+                            :with_complex_version) }
+  let(:presenter) { Hyrax::ImagePresenter.new(SolrDocument.new(image.to_solr), Ability.new(user)) }
 
   before do
     allow(controller).to receive(:current_user).and_return(user)
@@ -30,16 +30,9 @@ RSpec.describe 'hyrax/publications/_attribute_rows' do
       expect(rendered).to have_content('Rights-Statement-123')
       expect(rendered).to have_content('28/05/2019') # NB: complex date is reformatted to dd/mm/yyyy
       expect(rendered).to have_content('10.0.1111')
-      expect(rendered).to have_content('Big Baz')
+      expect(rendered).to have_content('Complex-Person-123')
       expect(rendered).to have_content('http://creativecommons.org/publicdomain/zero/1.0/')
-      expect(rendered).to have_content('Creating the first version')
-      expect(rendered).to have_content('Event-Title-123')
-      expect(rendered).to have_content('Issue-123')
-      expect(rendered).to have_content('221B Baker Street Place')
-      expect(rendered).to have_content('Table-of-Contents-123')
-      expect(rendered).to have_content('Number-of-Pages-123')
-      expect(rendered).to have_content('Source-123')
-      expect(rendered).to have_content('Test journal')
+      expect(rendered).to have_content('Complex-Version-123')
     end
   end
 
@@ -55,16 +48,9 @@ RSpec.describe 'hyrax/publications/_attribute_rows' do
       expect(rendered).to have_content('Rights-Statement-123')
       expect(rendered).to have_content('28/05/2019') # NB: complex date is reformatted to dd/mm/yyyy
       expect(rendered).to have_content('10.0.1111')
-      expect(rendered).to have_content('Big Baz')
+      expect(rendered).to have_content('Complex-Person-123')
       expect(rendered).to have_content('http://creativecommons.org/publicdomain/zero/1.0/')
-      expect(rendered).to have_content('Creating the first version')
-      expect(rendered).to have_content('Event-Title-123')
-      expect(rendered).to have_content('Issue-123')
-      expect(rendered).to have_content('221B Baker Street Place')
-      expect(rendered).to have_content('Table-of-Contents-123')
-      expect(rendered).to have_content('Number-of-Pages-123')
-      expect(rendered).to have_content('Source-123')
-      expect(rendered).to have_content('Test journal')
+      expect(rendered).to have_content('Complex-Version-123')
     end
   end
 end
