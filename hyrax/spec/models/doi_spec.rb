66670cd6b6f8de842202bef44b03b892324073e9
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-12-16 10:38:56 +0000

    Update doi_spec.rb

--- hyrax/spec/models/doi_spec.rb
+++ hyrax/spec/models/doi_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe DOI do
-  pending 'work in progress'
+  let(:doi) { described_class.new(value) }
+
+  shared_examples 'doi' do
+    context 'url' do
+      subject { doi.url }
+      it { is_expected.to eql('https://doi.org/10.5555/12345678') }
+    end
+
+    context 'label' do
+      subject { doi.label }
+      it { is_expected.to eql('doi:10.5555/12345678') }
+    end
+  end
+
+  context 'http:// prefix' do
+    context 'with dx.' do
+      let(:value) { 'http://dx.doi.org/10.5555/12345678' }
+      it_behaves_like 'doi'
+    end
+    context 'without dx.' do
+      let(:value) { 'http://doi.org/10.5555/12345678' }
+      it_behaves_like 'doi'
+    end
+  end
+
+  context 'https:// prefix' do
+    context 'with dx.' do
+      let(:value) { 'https://dx.doi.org/10.5555/12345678' }
+      it_behaves_like 'doi'
+    end
+    context 'without dx.' do
+      let(:value) { 'https://doi.org/10.5555/12345678' }
+      it_behaves_like 'doi'
+    end
+  end
+
+  context 'doi: prefix' do
+    context 'with whitespace' do
+      let(:value) { 'doi: 10.5555/12345678' }
+      it_behaves_like 'doi'
+    end
+    context 'without whitespace' do
+      let(:value) { 'doi:10.5555/12345678' }
+      it_behaves_like 'doi'
+    end
+  end
+
+  context 'no prefix' do
+    let(:value) { '10.5555/12345678' }
+    it_behaves_like 'doi'
+  end
 end
