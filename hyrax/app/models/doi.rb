f030018ad9e0573a6a87d6f90ed30a91b7e7fb60
Martyn Whitwell <martyn.whitwell@gmail.com>
2019-12-16 13:04:25 +0000

    handle handler

--- hyrax/app/models/doi.rb
+++ hyrax/app/models/doi.rb
@@ -3,21 +3,24 @@ class DOI
 
   DOI_URL_REGEXP = /^https?\:\/\/(?:dx\.)?doi\.org\/(.+)$/i
   DOI_PREFIX_REGEXP = /^doi\:(?:\s*)(.+)$/i
+  INFO_DOI_PREFIX_REGEXP = /^info\:(?:\s*)doi\/(.+)$/i
 
   def initialize(value)
     # Example valid DOIs which should be parsed:
     # * 10.5555/12345678
     # * doi:10.5555/12345678
+    # * info:doi/10.5555/12345678       # from RFC4452
     # * http://dx.doi.org/10.5555/12345678
     # * https://doi.org/10.5555/12345678
     # * 10/hvx
     # * doi:10/hvx
     # * http://doi.org/hvx
 
-    if match = value.match(DOI_URL_REGEXP) || value.match(DOI_PREFIX_REGEXP)
+    # check if the value has a doi: or url prefix, and if so, extract the raw doi
+    if (match = DOI.match_doi_prefix(value))
       @identifier = match.captures.first
     else
-      # the value is not a URL or prefixed with doi:, so just assume it is a raw doi
+      # the value is not a doi URL or prefixed with doi: or info:doi/, so just assume it is a raw doi
       @identifier = value
     end
   end
@@ -28,5 +31,10 @@ class DOI
 
   def label
     "doi:#{@identifier}"
+  end
+
+  def self.match_doi_prefix(value)
+    return nil unless value.is_a?(String) && value.present?
+    value.match(DOI_URL_REGEXP) || value.match(DOI_PREFIX_REGEXP) || value.match(INFO_DOI_PREFIX_REGEXP)
   end
 end
