83f3c8a1b695add127ac0ee934819bd09297bfdb
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-01-09 09:57:10 +0000

    test fixes

--- hyrax/spec/features/contact_form_spec.rb
+++ hyrax/spec/features/contact_form_spec.rb
@@ -4,24 +4,16 @@ include Warden::Test::Helpers
 # NOTE: If you generated more than one work, you have to set "js: true"
 RSpec.feature 'Show Contact form', js: false do
   context 'a logged in user' do
-    let(:user_attributes) do
-      { email: 'test@example.com' }
-    end
-    let(:user) do
-      User.new(user_attributes) { |u| u.save(validate: false) }
-    end
+    let(:user) { build(:user) }
 
     before do
       login_as user
     end
 
-    scenario do
+    scenario "should display contact form" do
       visit '/contact'
-
-      it "should display contact form" do
-        expect(page).to have_field 'Your Name', with: user.username, readonly: true
-        expect(page).to have_field 'Your Email', with: user.email, readonly: true
-      end
+      expect(page).to have_field 'Your Name', with: user.username, readonly: true
+      expect(page).to have_field 'Your Email', with: user.email, readonly: true
     end
   end
 end
