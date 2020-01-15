0e92bdff142b50845dc5e6926312d2b2a6a70323
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-01-15 15:52:38 +0000

    single sign-out patches

--- hyrax/config/initializers/session_store.rb
+++ hyrax/config/initializers/session_store.rb
@@ -1,3 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hyrax_session'
+# NB the cookie session store is not compatible with single sign out - so we shall use a Redis-based session store instead (redis-session-store gem)
+redis_config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+Rails.application.config.session_store :redis_session_store, {
+    key: '_hyrax_session',
+    redis: {
+        expire_after: 90.minutes,  # cookie expiration
+        ttl: 90.minutes,           # Redis expiration, defaults to 'expire_after'
+        key_prefix: 'hyrax:session:',
+        url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0",
+    }
+}
+
+# monkey patch single sign out method
+require_relative '../../lib/extensions/devise_cas_authenticatable/single_sign_out/single_sign_out'
---
+++ hyrax/lib/extensions/devise_cas_authenticatable/single_sign_out/single_sign_out.rb
@@ -0,0 +1,23 @@
+# Monkey Patch to DeviseCasAuthenticatable::SingleSignOut::DestroySession#destroy_session_by_id to fix env issue
+
+module Extensions
+  module DeviseCasAuthenticatable
+    module SingleSignOut
+      module DestroySession
+        def destroy_session_by_id(sid)
+          logger.debug "Single Sign Out from session store: #{current_session_store.class}"
+          if session_store_class.name =~ /RedisSessionStore/
+            current_session_store.send(:destroy_session, {}, sid, drop: true)
+            true
+          else
+            logger.error "Unsupported session store: #{session_store_class.name}"
+            false
+          end
+        end
+      end
+    end
+  end
+end
+
+# monkey patch DestroySession
+::DeviseCasAuthenticatable::SingleSignOut::DestroySession.include Extensions::DeviseCasAuthenticatable::SingleSignOut::DestroySession
