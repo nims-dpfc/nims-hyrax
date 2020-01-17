6ef3b3108655491678e5d91e87fd7cd71bfd2e97
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-01-17 10:50:49 +0000

    temporarily disable apis
    see issue #241

--- hyrax/app/controllers/hyrax/images_controller.rb
+++ hyrax/app/controllers/hyrax/images_controller.rb
@@ -6,6 +6,8 @@ module Hyrax
     # Adds Hyrax behaviors to the controller.
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
+    prepend ::DisableApiBehavior
+
     self.curation_concern_type = ::Image
 
     # Use this line if you want to use a custom presenter
