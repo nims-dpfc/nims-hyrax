b1e505b94174a4cd5a7bab3f9768ac227ce65f89
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-07 20:37:54 +0000

    wip

--- hyrax/app/jobs/synchronise_pids_job.rb
+++
@@ -1,9 +0,0 @@
-# Synchronises with the NIMS PID API
-require "json-api-vanilla"
-class SynchronisePIDsJob < ApplicationJob
-  # @param work [ActiveFedora::Base] the work object
-  def perform(work)
-    # not sure what to do here
-    puts "Synchronising PIDs for work: #{work.inspect}"
-  end
-end
