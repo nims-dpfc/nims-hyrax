c1ad22f1c153d8439c6466b6fa75db5f7b69f2fb
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-12 23:51:02 +0000

    more dataset presenter tests
    now checks ttl, nt and jsonld

--- hyrax/spec/presenters/hyrax/dataset_presenter_spec.rb
+++ hyrax/spec/presenters/hyrax/dataset_presenter_spec.rb
@@ -1,78 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::DatasetPresenter do
-  let(:dataset) { create(:dataset, :open, :with_alternative_title, :with_description_abstract) }
+  let(:dataset) { create(:dataset, :open, :with_alternative_title, :with_description_abstract, :with_supervisor_approval) }
   let(:solr_document) { SolrDocument.new(dataset.to_solr) }
-  let(:host) { double(host: 'http://example.org/') }
-
+  let(:host) { double(host: 'http://example.org') }
+  let(:user) { nil }
   let(:presenter) { described_class.new(solr_document, Ability.new(user), host) }
-
-  let(:ttl_regex) {
-    [
-      %r(<http://example.org//concern/datasets/#{dataset.id}> a),
-      %r(<http://projecthydra.org/works/models#Work>),
-      %r(<http://pcdm.org/models#Object>),
-      %r(<http://purl.org/dc/terms/title> "Open Dataset";),
-      %r(<http://purl.org/dc/terms/alternative> "Alternative-Title-123";),
-      %r(<http://www.w3.org/ns/auth/acl#accessControl> <http://example.org//catalog/([a-f0-9\\-]*)+>;),
-      %r(<info:fedora/fedora-system:def/model#hasModel> "Dataset" .)
-    ]
-  }
-  let(:ttl_regex_abstract) { %r(<http://purl.org/dc/elements/1.1/description> "Abstract-Description-123";) }
 
   describe '#export_as_ttl' do
     subject { presenter.export_as_ttl }
-    let(:user) { nil }
-    it 'generates ttl' do
-      ttl_regex.each do |regex|
+    let(:export_regex) {
+      [
+        %r(<http://example.org/concern/datasets/#{dataset.id}> a),
+        %r(<http://projecthydra.org/works/models#Work>),
+        %r(<http://pcdm.org/models#Object>),
+        %r(<http://purl.org/dc/terms/title> "Open Dataset";),
+        %r(<http://purl.org/dc/terms/alternative> "Alternative-Title-123";),
+        %r(<http://www.w3.org/ns/auth/acl#accessControl> <http://example.org/catalog/([a-f0-9\\-]*)+>;),
+        %r(<info:fedora/fedora-system:def/model#hasModel> "Dataset" .)
+      ]
+    }
+    let(:abstract_regex) { %r(<http://purl.org/dc/elements/1.1/description> "Abstract-Description-123";) }
+    let(:supervisor_regex) { %r(<http://www.nims.go.jp/vocabs/ngdr/supervisor-approval> "Professor Supervisor Approval";) }
+
+    it 'exports' do
+      export_regex.each do |regex|
         expect(subject).to match(regex)
       end
     end
 
     context 'anonymous user' do
-      it { is_expected.not_to match(ttl_regex_abstract) }
+      it { is_expected.not_to match(abstract_regex) }
+      it { is_expected.not_to match(supervisor_regex) }
     end
 
     context 'authenticated user' do
       let(:user) { create(:user, :nims_other) }
-      it { is_expected.to match(ttl_regex_abstract) }
+      it { is_expected.to match(abstract_regex) }
+      it { is_expected.not_to match(supervisor_regex) }
     end
   end
 
+  describe '#export_as_nt' do
+    subject { presenter.export_as_nt }
+    let(:export_regex) {
+      [
+        %r(<http://example.org/concern/datasets/#{dataset.id}> <http://www.w3.org/ns/auth/acl#accessControl> <http://example.org/catalog/([a-f0-9\\-]*)+>),
+        %r(<http://example.org/concern/datasets/#{dataset.id}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://projecthydra.org/works/models#Work>),
+        %r(<http://example.org/concern/datasets/#{dataset.id}> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://pcdm.org/models#Object>),
+        %r(<http://example.org/concern/datasets/#{dataset.id}> <info:fedora/fedora-system:def/model#hasModel> "Dataset"),
+        %r(<http://example.org/concern/datasets/#{dataset.id}> <http://purl.org/dc/terms/alternative> "Alternative-Title-123"),
+        %r(<http://example.org/concern/datasets/#{dataset.id}> <http://purl.org/dc/terms/title> "Open Dataset")
+      ]
+    }
+    let(:abstract_regex) { %r(<http://example.org/concern/datasets/#{dataset.id}> <http://purl.org/dc/elements/1.1/description> "Abstract-Description-123") }
+    let(:supervisor_regex) { %r(<http://example.org/concern/datasets/#{dataset.id}> <http://www.nims.go.jp/vocabs/ngdr/supervisor-approval> "Professor Supervisor Approval") }
 
+    it 'exports' do
+      export_regex.each do |regex|
+        expect(subject).to match(regex)
+      end
+    end
 
+    context 'anonymous user' do
+      it { is_expected.not_to match(abstract_regex) }
+      it { is_expected.not_to match(supervisor_regex) }
+    end
 
+    context 'authenticated user' do
+      let(:user) { create(:user, :nims_other) }
+      it { is_expected.to match(abstract_regex) }
+      it { is_expected.not_to match(supervisor_regex) }
+    end
+  end
 
+  describe '#export_as_jsonld' do
+    subject { JSON.parse(presenter.export_as_jsonld) }
+    # NB: it is important to use => rather than a colon: - otherwise the strings get symbolised
 
-  # # let(:graph) { Hyrax::GraphExporter.new(solr_document, host).fetch }
-  #
-  # let(:ttl) { graph.dump(:ttl) }
-  # let(:dmp) {  graph.each_statement {|s| puts "SUBJ: #{s.subject}\tPRED:#{s.predicate}\tOBJ: #{s.object}" } }
-  #
-  # # let(:replacer) { ->(id, _graph) {
-  # #     puts "id: #{id.inspect}\n _graph: #{_graph.inspect}"
-  # #   }
-  # # }
-  #
-  #
-  # #let(:connection) { Hyrax::CleanConnection.new(ActiveFedora.fedora.connection) }
-  # #let(:clean_graph_repository) { Hydra::ContentNegotiation::CleanGraphRepository.new(connection, replacer) }
-  # #let(:clean_graph) { clean_graph_repository.find(solr_document.id) }
-  #
-  # let(:clean_graph) {
-  #   RDF::Graph.new.insert(*graph.each_statement.to_a.reject { |statement| statement.predicate == 'http://purl.org/dc/elements/1.1/description' })
-  # }
-  #
-  # let(:clean_ttl) { clean_graph.dump(:ttl) }
-  # let(:clean_dmp) {  clean_graph.each_statement {|s| puts "SUBJ: #{s.subject}\tPRED:#{s.predicate}\tOBJ: #{s.object}" } }
-  #
-  #
-  # it "filters abstract for anonymous users" do
-  #   # puts graph.inspect
-  #   require 'byebug'
-  #   byebug
-  #
-  #
-  #
-  # end
+    it 'exports' do
+      expect(subject["@context"]).to include(
+        "pcdmterms" => "http://pcdm.org/models#",
+        "worksterms" => "http://projecthydra.org/works/models#",
+        "dc" => "http://purl.org/dc/terms/",
+        "acl" => "http://www.w3.org/ns/auth/acl#",
+        "system" => "info:fedora/fedora-system:",
+        "model" => "system:def/model#"
+      )
+      expect(subject["@id"]).to eql "http://example.org/concern/datasets/#{dataset.id}"
+      expect(subject["dc:title"]).to eql "Open Dataset"
+      expect(subject["dc:alternative"]).to eql "Alternative-Title-123"
+      expect(subject["model:hasModel"]).to eql "Dataset"
+      expect(subject["acl:accessControl"]).to include("@id")
+      expect(subject["@type"]).to match_array %w(pcdmterms:Object worksterms:Work)
+    end
+
+    context 'anonymous user' do
+      it { is_expected.not_to include("dc11:description" => "Abstract-Description-123") }
+      it { is_expected.not_to include("nimsrdp:supervisor-approval" => "Professor Supervisor Approval") }
+    end
+
+    context 'authenticated user' do
+      let(:user) { create(:user, :nims_other) }
+      it { is_expected.to include("dc11:description" => "Abstract-Description-123") }
+      it { is_expected.not_to include("nimsrdp:supervisor-approval" => "Professor Supervisor Approval") }
+    end
+  end
 end
