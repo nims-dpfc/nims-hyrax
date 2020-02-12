77dbf02e0e007ec9324bb5466f86a25986b1462d
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-12 15:21:38 +0000

    Make NIMS PID readonly on edit form

--- hyrax/app/inputs/nested_identifier_input.rb
+++ hyrax/app/inputs/nested_identifier_input.rb
@@ -18,37 +18,51 @@ protected
     # --- scheme and id - single row
     out << "<div class='row'>"
 
-    # --- scheme
-    field = :scheme
-    field_name = name_for(attribute_name, index, field, parent)
-    field_id = id_for(attribute_name, index, field, parent)
-    field_value = value.send(field).first
-    id_options = IdentifierService.new.select_all_options
+    scheme_field = :scheme
+    scheme_field_name = name_for(attribute_name, index, scheme_field, parent)
+    scheme_field_id = id_for(attribute_name, index, scheme_field, parent)
+    scheme_field_value = value.send(scheme_field).first
 
-    out << "  <div class='col-md-3'>"
-    out << template.select_tag(field_name,
-        template.options_for_select(id_options, field_value),
-        label: '', class: 'select form-control', prompt: 'choose type', id: field_id)
-    out << '  </div>'
+    identifier_field = :identifier
+    identifier_field_name = name_for(attribute_name, index, identifier_field, parent)
+    identifier_field_id = id_for(attribute_name, index, identifier_field, parent)
+    identifier_field_value = value.send(identifier_field).first
 
-    # --- identifier
-    field = :identifier
-    field_name = name_for(attribute_name, index, field, parent)
-    field_id = id_for(attribute_name, index, field, parent)
-    field_value = value.send(field).first
-
-    out << "  <div class='col-md-6'>"
-    out << @builder.text_field(field_name,
-        options.merge(value: field_value, name: field_name, id: field_id,
-            required: required))
-    out << '  </div>'
-
-    # --- delete checkbox
-    if repeats == true
-      field_label = 'Identifier'
+    # check if the identifier is a NIMS PID - if so, it should be displayed as read only
+    if scheme_field_value =~ /identifier persistent/i && identifier_field_value =~ /urn\:(DATA|USER)_IDENTIFIER\.dpfc\.nims\.go\.jp\:([a-f0-9\-]+)/i
+      # --- scheme
       out << "  <div class='col-md-3'>"
-      out << destroy_widget(attribute_name, index, field_label, parent)
+      out << IdentifierService.new.find_by_id(scheme_field_value)[:label]
+      out << @builder.hidden_field(scheme_field_name, options.merge(value: scheme_field_value, name: scheme_field_name, id: scheme_field_id))
       out << '  </div>'
+
+      # --- identifier
+      out << "  <div class='col-md-9'>"
+      out << "  <code>#{identifier_field_value}</code>"
+      out << @builder.hidden_field(identifier_field_name, options.merge(value: identifier_field_value, name: identifier_field_name, id: identifier_field_id))
+      out << '  </div>'
+    else
+      # --- scheme
+      id_options = IdentifierService.new.select_all_options
+      out << "  <div class='col-md-3'>"
+      out << template.select_tag(scheme_field_name,
+                                 template.options_for_select(id_options, scheme_field_value),
+                                 label: '', class: 'select form-control', prompt: 'choose type', id: scheme_field_id)
+      out << '  </div>'
+
+      # --- identifier
+      out << "  <div class='col-md-6'>"
+      out << @builder.text_field(identifier_field_name,
+                                 options.merge(value: identifier_field_value, name: identifier_field_name, id: identifier_field_id,
+                                               required: required))
+      out << '  </div>'
+
+      # --- delete checkbox
+      if repeats == true
+        out << "  <div class='col-md-3'>"
+        out << destroy_widget(attribute_name, index, 'Identifier', parent)
+        out << '  </div>'
+      end
     end
 
     out << '</div>' # last row
