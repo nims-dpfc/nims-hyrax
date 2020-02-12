9f13158f6a4b0fd1c4a08fc50ae9539d082ddfbc
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-12 16:05:23 +0000

    tidy up some debugs

--- hyrax/app/jobs/get_pid_job.rb
+++ hyrax/app/jobs/get_pid_job.rb
@@ -4,40 +4,30 @@ require 'net/http'
 class GetPIDJob < ApplicationJob
   # @param work [ActiveFedora::Base] the work object
 
-
-
   def perform(work)
     raise('Missing PID_API_URL env var') unless ENV['PID_API_URL'].present?
     raise('Missing PID_API_AUTHORIZATION env var') unless ENV['PID_API_AUTHORIZATION'].present?
 
-    puts "Getting PID for work: #{work.inspect}"
+    puts "Getting PID for work: #{work.id}"
 
     work_url = routes.send(work.model_name.singular_route_key + '_url', work.id)
 
     pidrequest = PIDRequest.new(
         id: work.id,
         localId: work_url,
-        creator: { canonicalId: "urn:USER_IDENTIFIER.dpfc.nims.go.jp:c8e6baaf-3cc7-4d21-86fd-3ffd8193f688" }, # #{work.depositor} TODO: get uuid of user
+        creator: { canonicalId: "urn:USER_IDENTIFIER.dpfc.nims.go.jp:c8e6baaf-3cc7-4d21-86fd-3ffd8193f688" }, # TODO: get NIMS PID of user when developed (see https://github.com/antleaf/nims-mdr-development/issues/226#issuecomment-584936488)
         pidCategory: 'DATA_IDENTIFIER',
         disclosureLevel:'PRIVATE'
     )
 
     doc = renderer.render(pidrequest, class: { PIDRequest: PIDRequestSerializer })
-
-    puts "DOC: #{doc.inspect}"
-
     response = client.post(ENV['PID_API_URL'], doc.to_json)
-
-    puts "RESPONSE: #{response.inspect}"
 
     if response.success?
       pid_response = PIDResponseDeserializer.call(JSON.parse(response.body)['data'])
-      puts "PID_CANONICAL_ID: #{pid_response[:canonicalId]}" #  "urn:DATA_IDENTIFIER.dpfc.nims.go.jp:f4415bd4-edc0-4a97-a2e2-3ecd02e551bd"
-
-      work.complex_identifier_attributes = [
-          { identifier: pid_response[:id], scheme: ['identifier persistent'] },
-          { identifier: pid_response[:canonicalId], scheme: ['identifier persistent'] }
-      ]
+      puts "Got PID for work: #{work.id}: #{pid_response[:canonicalId]}"
+      # NB: this appends the PID to any existing identifiers
+      work.complex_identifier_attributes = [ { identifier: pid_response[:canonicalId], scheme: ['identifier persistent'] }]
       work.save
     else
       begin
@@ -49,7 +39,6 @@ class GetPIDJob < ApplicationJob
       puts "Error calling PID API (#{response.status}): #{error_msg}"
       raise "Call to PID API failed (#{response.status})"
     end
-
   end
 
   private
--- hyrax/app/services/workflow/get_pid.rb
+++ hyrax/app/services/workflow/get_pid.rb
@@ -1,9 +1,6 @@
 module Workflow
   module GetPID
     def self.call(target:, **)
-      puts "Calling GetPIDjob later:"
-      puts "TARGET: #{target.inspect}"
-
       GetPIDJob.perform_later(target)
     end
   end
