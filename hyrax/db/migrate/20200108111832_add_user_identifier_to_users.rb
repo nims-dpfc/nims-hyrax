26f6b1e821df4e58f8538c687a94568a26462bf8
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2020-02-17 10:48:44 +0900

    add unique index to user_identifier, add PIDs to default users

--- hyrax/db/migrate/20200108111832_add_user_identifier_to_users.rb
+++ hyrax/db/migrate/20200108111832_add_user_identifier_to_users.rb
@@ -1,6 +1,6 @@
 class AddUserIdentifierToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :user_identifier, :string
-    add_index :users, :user_identifier
+    add_column :users, :user_identifier, :string #, null: false
+    add_index :users, :user_identifier, unique: true
   end
 end
