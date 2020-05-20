a21fe265730c3b39c1b820708e84aa99a7c8b6d3
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2020-05-20 12:22:29 +0900

    add last_name and first_name fields to ComplexPerson form

--- hyrax/app/inputs/nested_person_input.rb
+++ hyrax/app/inputs/nested_person_input.rb
@@ -17,6 +17,40 @@ protected
 
     parent_attribute = name_for(attribute_name, index, '', parent)[0..-5]
 
+    # --- last_name
+    field = :last_name
+    field_name = name_for(attribute_name, index, field, parent)
+    field_id = id_for(attribute_name, index, field, parent)
+    field_value = value.send(field).first
+
+    out << "<div class='row'>"
+    out << "  <div class='col-md-3'>"
+    out << template.label_tag(field_name, I18n.t('ngdr.fields.last_name'), required: required)
+    out << '  </div>'
+
+    out << "  <div class='col-md-9'>"
+    out << @builder.text_field(field_name,
+        options.merge(value: field_value, name: field_name, id: field_id, required: required))
+    out << '  </div>'
+    out << '</div>' # row
+
+    # --- first_name
+    field = :first_name
+    field_name = name_for(attribute_name, index, field, parent)
+    field_id = id_for(attribute_name, index, field, parent)
+    field_value = value.send(field).first
+
+    out << "<div class='row'>"
+    out << "  <div class='col-md-3'>"
+    out << template.label_tag(field_name, I18n.t('ngdr.fields.first_name'), required: required)
+    out << '  </div>'
+
+    out << "  <div class='col-md-9'>"
+    out << @builder.text_field(field_name,
+        options.merge(value: field_value, name: field_name, id: field_id, required: required))
+    out << '  </div>'
+    out << '</div>' # row
+
     # --- name
     field = :name
     field_name = name_for(attribute_name, index, field, parent)
