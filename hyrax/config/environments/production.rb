837ae60b4786e70182ad6b7672cf45fef2f27087
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2020-06-08 14:50:42 +0900

    remove "ignore_if" option from exception_notification

--- hyrax/config/environments/production.rb
+++ hyrax/config/environments/production.rb
@@ -114,7 +114,6 @@ Rails.application.configure do
   }
 
   config.middleware.use ExceptionNotification::Rack,
-    ignore_if: lambda { |env, exception| !env.nil? },
     error_grouping: true,
     email: {
       email_prefix: "[MDR #{ENV['ERROR_NOTIFICATION_SUBJECT_PREFIX']}] ",
