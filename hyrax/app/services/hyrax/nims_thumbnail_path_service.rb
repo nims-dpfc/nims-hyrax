32c2d2ac055ef97d330d400c68512e814b1d2435
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2020-06-12 02:17:07 +0900

    add Hyrax::NimsThumbnailPathService module and mdr-default.png

---
+++ hyrax/app/services/hyrax/nims_thumbnail_path_service.rb
@@ -0,0 +1,23 @@
+#require_dependency Rails.root.join('app/services/hyrax/work_thumbnail_path_service')
+
+module Hyrax
+  module NimsThumbnailPathService
+    def default_image
+      ActionController::Base.helpers.image_path 'mdr-default.png'
+    end
+  end
+end
+
+class Hyrax::ThumbnailPathService
+  class << self
+    prepend Hyrax::NimsThumbnailPathService
+  end
+end
+
+class Hyrax::WorkThumbnailPathService
+  class << self
+    prepend Hyrax::NimsThumbnailPathService
+  end
+end
+
+#Hyrax::ThumbnailPathService.singleton_class.prepend Hyrax::NimsThumbnailPathService
