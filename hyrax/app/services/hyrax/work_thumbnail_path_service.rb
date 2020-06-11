56869d8bef784244b8654e77c93d033f81ce085d
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2020-06-12 02:17:07 +0900

    add Hyrax::NimsThumbnailPathService module and mdr-default.png

--- hyrax/app/services/hyrax/work_thumbnail_path_service.rb
+++
@@ -1,9 +0,0 @@
-module Hyrax
-  class WorkThumbnailPathService < Hyrax::ThumbnailPathService
-    class << self
-      def default_image
-        ActionController::Base.helpers.image_path 'final-icons-01-100.png'
-      end
-    end
-  end
-end
