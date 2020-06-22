ad691277b53dd2ffcac15bfd1a3be2f05d6651e3
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2020-06-23 02:31:38 +0900

    ignore I18n::InvalidLocale in exception_notification

--- hyrax/config/environments/production.rb
+++ hyrax/config/environments/production.rb
@@ -114,6 +114,7 @@ Rails.application.configure do
   }
 
   config.middleware.use ExceptionNotification::Rack,
+    ignore_exceptions: ['I18n::InvalidLocale'] + ExceptionNotifier.ignored_exceptions,
     error_grouping: true,
     email: {
       email_prefix: "[MDR #{ENV['ERROR_NOTIFICATION_SUBJECT_PREFIX']}] ",
