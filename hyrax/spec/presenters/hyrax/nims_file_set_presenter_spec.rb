10e5eec7a4bd7f06929cd1b63ace675510bceccc
Rob Kaufman <rob@notch8.com>
2020-07-22 00:31:37 -0700

    add support for tsv files to datatables viewer

--- hyrax/spec/presenters/hyrax/nims_file_set_presenter_spec.rb
+++ hyrax/spec/presenters/hyrax/nims_file_set_presenter_spec.rb
@@ -20,19 +20,39 @@ RSpec.describe Hyrax::NimsFileSetPresenter do
     subject { presenter.csv? }
     context 'text/csv' do
       let(:mime_type) { 'text/csv' }
-      it { is_expected.to be true }
+      it { is_expected.to be_truthy }
     end
     context 'APPLICATION/CSV' do
       let(:mime_type) { 'APPLICATION/CSV' }
-      it { is_expected.to be true }
+      it { is_expected.to be_truthy }
     end
     context 'some/mime_type' do
       let(:mime_type) { 'some/mime_type' }
-      it { is_expected.to be false }
+      it { is_expected.to be_falsey }
     end
     context 'no mime type' do
       let(:mime_type) { nil }
-      it { is_expected.to be false }
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe 'tsv?' do
+    subject { presenter.tsv? }
+    context 'text/tab-separated-values' do
+      let(:mime_type) { 'text/tab-separated-values' }
+      it { is_expected.to be_truthy }
+    end
+    context 'APPLICATION/TAB-SEPARATED-VALUES' do
+      let(:mime_type) { 'APPLICATION/TAB-SEPARATED-VALUES' }
+      it { is_expected.to be_truthy }
+    end
+    context 'some/mime_type' do
+      let(:mime_type) { 'some/mime_type' }
+      it { is_expected.to be_falsey }
+    end
+    context 'no mime type' do
+      let(:mime_type) { nil }
+      it { is_expected.to be_falsey }
     end
   end
 end
