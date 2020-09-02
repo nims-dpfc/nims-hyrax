ac76405594f886bfdc5301c4322691dddec855bb
Steve Eardley <steve@eardley.xyz>
2020-09-02 21:59:59 +0100

    Updates rails to 5.2 and hyrax to 2.9, with changes to tests required to make them pass again.

--- hyrax/spec/presenters/hyrax/dataset_presenter_spec.rb
+++ hyrax/spec/presenters/hyrax/dataset_presenter_spec.rb
@@ -98,7 +98,7 @@ RSpec.describe Hyrax::DatasetPresenter do
       expect(subject["dc:alternative"]).to eql "Alternative-Title-123"
       expect(subject["model:hasModel"]).to eql "Dataset"
       expect(subject["acl:accessControl"]).to include("@id")
-      expect(subject["@type"]).to match_array %w(pcdmterms:Object worksterms:Work)
+      expect(subject["@type"]).to match_array %w(pcdm:Object worksterms:Work)
     end
 
     context 'anonymous user' do
