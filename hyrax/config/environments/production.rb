047427cbcfc6c710e1b642cc2546958347d2ef65
Steve Eardley <steve@eardley.xyz>
2020-09-03 08:57:14 +0100

    Add secret key to config for rails 5.2

--- hyrax/config/environments/production.rb
+++ hyrax/config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Added for Rails 5.2
+  config.secret_key_base = ENV['SECRET_KEY_BASE_PRODUCTION']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
