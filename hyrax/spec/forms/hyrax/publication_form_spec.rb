06d86464aaf1f475a26255c85d93d93e1f784b29
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2020-12-22 00:59:57 +0900

    add complex_relation to Publication

--- hyrax/spec/forms/hyrax/publication_form_spec.rb
+++ hyrax/spec/forms/hyrax/publication_form_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Hyrax::PublicationForm do
       it do
         expect(described_class).to receive(:permitted_identifier_params).at_least(:once).and_call_original
         expect(described_class).to receive(:permitted_person_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_relation_params).at_least(:once).and_call_original
         expect(described_class).to receive(:permitted_version_params).at_least(:once).and_call_original
         expect(described_class).to receive(:permitted_event_params).at_least(:once).and_call_original
         expect(described_class).to receive(:permitted_source_params).at_least(:once).and_call_original
