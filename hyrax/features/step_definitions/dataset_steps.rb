d30cc4b294788e057edcbc5964564583601c989a
Asahiko Matsuda <689506+asahiko@users.noreply.github.com>
2020-12-22 18:59:15 +0900

    Fix content expectation when choosing open visibility

--- hyrax/features/step_definitions/dataset_steps.rb
+++ hyrax/features/step_definitions/dataset_steps.rb
@@ -65,7 +65,7 @@ When(/^I create the dataset with:$/) do |table|
   # its element
   find('body').click
   choose('dataset_visibility_open')
-  expect(page).to have_content('Please note, making something visible to the world (i.e. marking this as Public) may be viewed as publishing which could impact your ability to')
+  expect(page).to have_content('Please note, making something visible to the world (i.e. marking this as MDR Open) may be viewed as publishing which could impact your ability to')
   check('agreement')
 
   click_on('Save')
