10a9c120f82ac4af16fd1ea34f1714a8afaeaaf0
Rob Kaufman <rob@notch8.com>
2020-12-21 17:42:30 -0800

    its getting drafty in here. fix the js based submit params

--- hyrax/app/assets/javascripts/application.js
+++ hyrax/app/assets/javascripts/application.js
@@ -24,3 +24,14 @@
 
 //= require_tree .
 //= require hyrax
+
+
+// Patch hyrax form submit to keep the submit button values
+// so that they get to Rails even when they are disabled
+$(document).ready(function(){
+  $("form[data-behavior='work-form'] input[type=submit]").click(function (e) {
+    var button = $(this);
+    var form = button.parents('form')[0]
+    $(form).append(`<input type="hidden" id="${this.id}-hidden" name="${this.name}" value="${this.value}"></input>`)
+  });
+})
