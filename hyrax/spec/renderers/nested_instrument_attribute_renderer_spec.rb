ceacd9fea880311035cbbd05c28d34ace7a301b4
Kosuke Tanabe <nabeta@fastmail.fm>
2020-12-23 14:16:41 +0900

    fix spec files

--- hyrax/spec/renderers/nested_instrument_attribute_renderer_spec.rb
+++ hyrax/spec/renderers/nested_instrument_attribute_renderer_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe NestedInstrumentAttributeRenderer do
     is_expected.to have_css('div.row label', text: 'Alternative title')
     is_expected.to have_css('div.row', text: 'An instrument title')
 
-    is_expected.to have_css('div.row label', text: 'Published')
+    is_expected.to have_css('div.row label', text: 'Collected')
     is_expected.to have_css('div.row', text: '14/02/2018')
 
     is_expected.to have_css('div.row label', text: 'Description')
