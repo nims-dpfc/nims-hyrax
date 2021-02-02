4276906dc968a52c598abace1b3607f826239ffc
Kosuke Tanabe <nabeta@fastmail.fm>
2021-02-02 11:52:10 +0900

    clean up

--- hyrax/spec/views/shared/_footer.html_spec.rb
+++ hyrax/spec/views/shared/_footer.html_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.describe 'shared/_footer' do
-  let(:dataset) { create(:dataset, :open, :with_description_seq) }
+  let(:dataset) { create(:dataset, :open) }
   let(:presenter) { Hyrax::DatasetPresenter.new(SolrDocument.new(dataset.to_solr), Ability.new(user), controller.request) }
 
   before do
@@ -10,10 +10,8 @@ RSpec.describe 'shared/_footer' do
     login_as user if user.present?
     assign(:presenter, presenter)
     render
-    render inline: '<%= yield :twitter_meta %>'
   end
 
-  # This test confirms the abstract is only visible if you are logged in
   describe 'copyright' do
     context 'unauthenticated user' do
       let(:user) { nil }
