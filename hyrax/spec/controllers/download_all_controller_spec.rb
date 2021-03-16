1e527854d242208e593f54125e46762bfd9dc7d3
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2021-03-16 01:25:20 +0000

    Attempt at writing test

--- hyrax/spec/controllers/download_all_controller_spec.rb
+++ hyrax/spec/controllers/download_all_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe DownloadAllController, type: :controller do
     context 'with public file_set' do
       before do
         allow(subject).to receive(:authorize_download!).and_return(true)
-        allow(subject).to receive(:available_file_set_ids).and_return([file_set.id])
+        allow(subject).to receive(:file_set_ids).and_return([file_set.id])
         CharacterizeJob.perform_now(file_set, file_set.original_file.id)
       end
 
@@ -31,11 +31,33 @@ RSpec.describe DownloadAllController, type: :controller do
       end
     end
 
+    context 'with more than 10 file_sets' do
+      before do
+        @file_set_ids = []
+        @file_sets = []
+        12.times do |i|
+          file_set = create(:file_set)
+          @file_sets.append(file_set)
+          @file_set_ids.append(file_set.id)
+          CharacterizeJob.perform_now(file_set, file_set.original_file.id)
+        end
+        allow(subject).to receive(:authorize_download!).and_return(true)
+      end
+      let(:dataset) { create(:dataset, members: @file_sets) }
+
+
+      it 'it returns the file set ids' do
+        skip "This test doesn't work"
+        get :show, params: { id: dataset.id, format: :zip }
+        expect(subject).to receive(:file_set_ids).and_return(@file_set_ids)
+      end
+    end
+
     context 'without file_sets' do
       let(:dataset) { create(:dataset) }
 
       before do
-        allow(subject).to receive(:available_file_set_ids).and_return([])
+        allow(subject).to receive(:file_set_ids).and_return([])
       end
 
       context 'request application/zip but without filesets' do
@@ -61,7 +83,7 @@ RSpec.describe DownloadAllController, type: :controller do
     context 'with a long filename' do
       before do
         allow(subject).to receive(:authorize_download!).and_return(true)
-        allow(subject).to receive(:available_file_set_ids).and_return([file_set.id])
+        allow(subject).to receive(:file_set_ids).and_return([file_set.id])
         CharacterizeJob.perform_now(file_set, file_set.original_file.id)
       end
 
