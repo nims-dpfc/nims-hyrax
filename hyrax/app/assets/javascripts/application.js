d843d3d352dea114caea4250f27ed64e9a8e71a0
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2021-04-09 11:22:43 +0900

    disable turbolinks

--- hyrax/app/assets/javascripts/application.js
+++ hyrax/app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
+// require turbolinks
 //
 //= require jquery
 //= require jquery_ujs
