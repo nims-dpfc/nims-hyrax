eaeac890f084ff5b1ca52f634c3a586d4ed23485
Kosuke Tanabe <nabeta@fastmail.fm>
2021-05-01 02:55:53 +0900

    remove Image model

--- hyrax/spec/actors/hyrax/actors/image_actor_spec.rb
+++
@@ -1,7 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Image`
-require 'rails_helper'
-
-RSpec.describe Hyrax::Actors::ImageActor do
-  it { expect(described_class).to be < Hyrax::Actors::BaseActor }
-end
