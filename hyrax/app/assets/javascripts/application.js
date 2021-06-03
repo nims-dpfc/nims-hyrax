b50f66f5fa50eff825a117ea2c17b105fb2de6c1
Anusha Ranganathan <anusha3@gmail.com>
2021-06-03 20:09:31 +0100

    Remove turbolinks

--- hyrax/app/assets/javascripts/application.js
+++ hyrax/app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
-//
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
