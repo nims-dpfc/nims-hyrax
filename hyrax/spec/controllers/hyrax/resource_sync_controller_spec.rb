b08e806513f266cc537fe248023587a74198c9c0
Kosuke Tanabe <nabeta@fastmail.fm>
2021-09-10 11:36:35 +0900

    fix spec file

--- hyrax/spec/controllers/hyrax/resource_sync_controller_spec.rb
+++ hyrax/spec/controllers/hyrax/resource_sync_controller_spec.rb
@@ -4,22 +4,26 @@ RSpec.describe Hyrax::ResourceSyncController do
   routes { Hyrax::Engine.routes }
 
   it "should not get source_description" do
-    get :source_description
-    expect(response).to be_not_found
+    expect {
+      get :source_description
+    }.to raise_error(ActionController::RoutingError)
   end
 
   it "should not get capability_list" do
-    get :capability_list
-    expect(response).to be_not_found
+    expect {
+      get :capability_list
+    }.to raise_error(ActionController::RoutingError)
   end
 
   it "should not get change_list" do
-    get :change_list
-    expect(response).to be_not_found
+    expect {
+      get :change_list
+    }.to raise_error(ActionController::RoutingError)
   end
 
   it "should not get resource_list" do
-    get :resource_list
-    expect(response).to be_not_found
+    expect {
+      get :resource_list
+    }.to raise_error(ActionController::RoutingError)
   end
 end
