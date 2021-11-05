22211a740b3af36fcdc23832dd7180592e90e39d
Kosuke Tanabe <nabeta@fastmail.fm>
2021-11-05 13:21:41 +0900

    update spec file

--- hyrax/spec/inputs/nested_specimen_type_input_spec.rb
+++ hyrax/spec/inputs/nested_specimen_type_input_spec.rb
@@ -26,12 +26,6 @@ RSpec.describe NestedSpecimenTypeInput, type: :input do
 
     is_expected.to have_field('dataset[complex_specimen_type_attributes][0]_complex_purchase_record_attributes_0_title', type: :text, with: 'Purchase record title')
     is_expected.to have_field('dataset[complex_specimen_type_attributes][0]_complex_purchase_record_attributes_0_date', type: :text, with: '2018-02-14')
-    is_expected.to have_field('dataset[complex_specimen_type_attributes][0][complex_purchase_record_attributes][0]_supplier_attributes_0_organization', type: :text, with: 'Fooss')
-    is_expected.to have_field('dataset[complex_specimen_type_attributes][0][complex_purchase_record_attributes][0]_supplier_attributes_0_sub_organization', type: :text, with: 'Barss')
-    is_expected.to have_field('dataset[complex_specimen_type_attributes][0][complex_purchase_record_attributes][0]_supplier_attributes_0_purpose', type: :text, with: 'Supplier')
-    is_expected.to have_field('dataset[complex_specimen_type_attributes][0][complex_purchase_record_attributes][0]_manufacturer_attributes_0_organization', type: :text, with: 'Foo')
-    is_expected.to have_field('dataset[complex_specimen_type_attributes][0][complex_purchase_record_attributes][0]_manufacturer_attributes_0_sub_organization', type: :text, with: 'Bar')
-    is_expected.to have_field('dataset[complex_specimen_type_attributes][0][complex_purchase_record_attributes][0]_manufacturer_attributes_0_purpose', type: :text, with: 'Manufacturer')
     is_expected.to have_field('dataset[complex_specimen_type_attributes][0]_complex_purchase_record_attributes_0_purchase_record_item', type: :text, with: 'Has a purchase record item')
 
     is_expected.to have_field('dataset[complex_specimen_type_attributes][0]_complex_structural_feature_attributes_0_category', type: :text, with: 'structural feature category')
