ee0c8197b873c9304a93e918aec6800649f6e769
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2022-05-30 22:00:09 +0900

    add a relationship between Dataset and ChemicalComposition

--- hyrax/app/indexers/complex_field/chemical_composition_indexer.rb
+++ hyrax/app/indexers/complex_field/chemical_composition_indexer.rb
@@ -7,19 +7,17 @@ module ComplexField
     end
 
     def index_chemical_composition(solr_doc)
-      object.complex_specimen_type.each do |st|
-        # description as complex_chemical_composition searchable
-        desc = st.complex_chemical_composition.map { |c| c.description.reject(&:blank?) }
-        fld_name = Solrizer.solr_name('complex_chemical_composition', :stored_searchable)
-        solr_doc[fld_name] = [] unless solr_doc.include?(fld_name)
-        solr_doc[fld_name] << desc
-        solr_doc[fld_name].flatten!
-        st.complex_chemical_composition.each do |cc|
-          cc.complex_identifier.each do |id|
-            fld_name = Solrizer.solr_name('complex_chemical_composition_identifier', :symbol)
-            solr_doc[fld_name] = [] unless solr_doc.include?(fld_name)
-            solr_doc[fld_name] << id.identifier.reject(&:blank?).first
-          end
+      solr_doc[Solrizer.solr_name('complex_chemical_composition', :displayable)] = object.complex_chemical_composition.to_json
+      fld_name = Solrizer.solr_name('complex_chemical_composition', :stored_searchable)
+      solr_doc[fld_name] = [] unless solr_doc.include?(fld_name)
+      solr_doc[fld_name] << object.complex_chemical_composition.map { |c| c.description.reject(&:blank?) }
+      solr_doc[fld_name].flatten!
+      object.complex_chemical_composition.each do |cc|
+        solr_doc[fld_name] << cc.category.reject(&:blank?).first
+        cc.complex_identifier.each do |id|
+          fld_name = Solrizer.solr_name('complex_chemical_composition_identifier', :symbol)
+          solr_doc[fld_name] = [] unless solr_doc.include?(fld_name)
+          solr_doc[fld_name] << id.identifier.reject(&:blank?).first
         end
       end
     end
@@ -32,5 +30,11 @@ module ComplexField
       fields
     end
 
+    def self.chemical_composition_show_fields
+      # solr fields that will be used to display results on the record page
+      fields = []
+      fields << Solrizer.solr_name('complex_chemical_composition', :displayable)
+      fields
+    end
   end
 end
