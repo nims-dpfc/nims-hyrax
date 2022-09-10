7c555d7c3da77d69ee6a5d50f43d99ef25d8e5d6
Kosuke Tanabe <nabeta@fastmail.fm>
2022-09-11 01:00:06 +0900

    add ComplexSoftware

---
+++ hyrax/app/inputs/nested_software_input.rb
@@ -0,0 +1,84 @@
+class NestedSoftwareInput < NestedAttributesInput
+
+protected
+
+  def build_components(attribute_name, value, index, options, parent=@builder.object_name)
+    out = ''
+
+    # Inherit required for fields validated in nested attributes
+    required  = false
+    if object.required?(:complex_source) and index == 0
+      required = true
+    end
+
+    # --- name
+    field = :name
+    field_name = name_for(attribute_name, index, field, parent)
+    field_id = id_for(attribute_name, index, field, parent)
+    field_value = value.send(field).first
+
+    out << "<div class='row'>"
+    out << "  <div class='col-md-3'>"
+    out << template.label_tag(field_name, field.to_s.humanize, required: required)
+    out << '  </div>'
+
+    out << "  <div class='col-md-9'>"
+    out << @builder.text_field(field_name,
+        options.merge(value: field_value, name: field_name, id: field_id, required: required, placeholder: "Journal name"))
+    out << '  </div>'
+    out << '</div>' # row
+
+    # --- version
+    field = :version
+    field_name = name_for(attribute_name, index, field, parent)
+    field_id = id_for(attribute_name, index, field, parent)
+    field_value = value.send(field).first
+
+    out << "<div class='row'>"
+    out << "  <div class='col-md-3'>"
+    out << template.label_tag(field_name, field.to_s.humanize, required: required)
+    out << '  </div>'
+
+    out << "  <div class='col-md-6'>"
+    out << @builder.text_field(field_name,
+        options.merge(value: field_value, name: field_name, id: field_id, required: required))
+    out << '  </div>'
+    out << '</div>' # row
+
+    # --- identifier
+    field = :identifier
+    field_name = name_for(attribute_name, index, field, parent)
+    field_id = id_for(attribute_name, index, field, parent)
+    field_value = value.send(field).first
+
+    out << "<div class='row'>"
+    out << "  <div class='col-md-3'>"
+    out << template.label_tag(field_name, field.to_s.humanize, required: false)
+    out << '  </div>'
+
+    out << "  <div class='col-md-3'>"
+    out << @builder.text_field(field_name,
+        options.merge(value: field_value, name: field_name, id: field_id, required: false))
+    out << '  </div>'
+    out << '</div>' # row
+
+    # --- description
+    field = :description
+    field_name = name_for(attribute_name, index, field, parent)
+    field_id = id_for(attribute_name, index, field, parent)
+    field_value = value.send(field).first
+
+    out << "<div class='row'>"
+    out << "  <div class='col-md-3'>"
+    out << template.label_tag(field_name, field.to_s.humanize, required: required)
+    out << '  </div>'
+
+    out << "  <div class='col-md-9'>"
+    out << @builder.text_field(field_name,
+        options.merge(value: field_value, name: field_name, id: field_id, required: required))
+    out << '  </div>'
+    out << '</div>' # row
+
+    out
+  end
+end
