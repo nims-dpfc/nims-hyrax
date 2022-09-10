c7fa65f96158be3f3c404e514ea92b02b0f01c19
Kosuke Tanabe <nabeta@fastmail.fm>
2022-09-11 01:12:55 +0900

    fix indexing

--- hyrax/app/indexers/complex_field/software_indexer.rb
+++ hyrax/app/indexers/complex_field/software_indexer.rb
@@ -2,11 +2,11 @@ module ComplexField
   module SoftwareIndexer
     def generate_solr_document
       super.tap do |solr_doc|
-        index_source(solr_doc)
+        index_software(solr_doc)
       end
     end
 
-    def index_source(solr_doc)
+    def index_software(solr_doc)
       solr_doc[Solrizer.solr_name('complex_software', :displayable)] = object.complex_software.to_json
       solr_doc[Solrizer.solr_name('complex_software_name', :stored_searchable)] = object.complex_software.map { |i| i.name.reject(&:blank?).first }
       solr_doc[Solrizer.solr_name('complex_software_name', :facetable)] = object.complex_software.map { |i| i.name.reject(&:blank?).first }
@@ -14,14 +14,14 @@ module ComplexField
       solr_doc[Solrizer.solr_name('complex_software_identifier', :stored_searchable)] = object.complex_software.map { |i| i.identifier.reject(&:blank?).first }
     end
 
-    def self.source_facet_fields
+    def self.software_facet_fields
       # solr fields that will be treated as facets
       fields = []
       fields << Solrizer.solr_name('complex_software_name', :facetable)
       fields
     end
 
-    def self.source_search_fields
+    def self.software_search_fields
       # solr fields that will be used for a search
       fields = []
       fields << Solrizer.solr_name('complex_software_name', :stored_searchable)
@@ -30,7 +30,7 @@ module ComplexField
       fields
     end
 
-    def self.source_show_fields
+    def self.software_show_fields
       # solr fields that will be used to display results on the record page
       fields = []
       fields << Solrizer.solr_name('complex_software', :displayable)
