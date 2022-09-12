f79f20af10e9d801366b0d40b30a39db38a8c9ad
Kosuke Tanabe <nabeta@fastmail.fm>
2022-09-12 18:54:21 +0900

    update spec files

--- hyrax/spec/forms/hyrax/dataset_form_spec.rb
+++ hyrax/spec/forms/hyrax/dataset_form_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe Hyrax::DatasetForm do
     describe '#method_tab_terms' do
       subject { form.method_tab_terms }
       it { is_expected.to include(:characterization_methods, :computational_methods, :properties_addressed, :synthesis_and_processing) }
+      it { is_expected.to include(:complex_feature) }
     end
 
     describe '#instrument_tab_terms' do
@@ -60,6 +61,7 @@ RSpec.describe Hyrax::DatasetForm do
         expect(described_class).to receive(:permitted_contact_agent_params).at_least(:once).and_call_original
         expect(described_class).to receive(:permitted_chemical_composition_params).at_least(:once).and_call_original
         expect(described_class).to receive(:permitted_structural_feature_params).at_least(:once).and_call_original
+        expect(described_class).to receive(:permitted_feature_params).at_least(:once).and_call_original
         subject
       end
     end
