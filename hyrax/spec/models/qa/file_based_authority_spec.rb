f426272ee7ebc4b346fa47b72cae96459016f160
Anusha Ranganathan <anusha@cottagelabs.com>
2022-09-13 04:43:54 +0100

    Fixed file based authority service terms and rights statement url update

--- hyrax/spec/models/qa/file_based_authority_spec.rb
+++ hyrax/spec/models/qa/file_based_authority_spec.rb
@@ -28,8 +28,30 @@ RSpec.describe Qa::Authorities::Local::FileBasedAuthority do
       it "can find a term for #{authority} given the id" do
         @term = @la.all.first
         @term['term'] = @term.delete('label')
-        expect(@la.find(@term['id'])).to eq (@term)
+        @term.delete('uri')
+        expect(@la.find(@term['id'])).to eq(@term)
       end
     end
   end
+
+  describe 'rights_statements' do
+    before do
+      @la = Qa::Authorities::Local::FileBasedAuthority.new('rights_statements')
+    end
+    it "has vocabulary for rights_statements" do
+      expect(@la.all).not_to be_nil
+      expect(@la.all.size).to be > 0
+    end
+    it "can find a term for rights_statements given the id" do
+      @term = @la.all.first
+      @term['term'] = @term.delete('label')
+      @term.delete('uri')
+      expect(@la.find(@term['id'])).to eq(@term)
+    end
+    it 'has the extra keys in terms hash for rights_statements' do
+      @term = @la.all.first
+      expect(@term.keys).to include('short_label', 'term', 'label', 'uri', 'human_url')
+    end
+  end
+
 end
