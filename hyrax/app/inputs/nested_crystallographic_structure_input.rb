e5bb5caaa9de55fefb1d36a119fec4c440e25f89
Kosuke Tanabe <nabeta@fastmail.fm>
2022-10-26 16:11:15 +0900

    add delete button

--- hyrax/app/inputs/nested_crystallographic_structure_input.rb
+++ hyrax/app/inputs/nested_crystallographic_structure_input.rb
@@ -61,13 +61,15 @@ class NestedCrystallographicStructureInput < NestedAttributesInput
     out << '  </div>'
     out << '</div>' # row
 
+    # last row
+    out << "<div class='row'>"
+
     # --- description
     field = :description
     field_name = name_for(attribute_name, index, field, parent)
     field_id = id_for(attribute_name, index, field, parent)
     field_value = value.send(field).first
 
-    out << "<div class='row'>"
     out << "  <div class='col-md-3'>"
     out << template.label_tag(field_name, field.to_s.humanize, required: required)
     out << '  </div>'
@@ -76,8 +78,16 @@ class NestedCrystallographicStructureInput < NestedAttributesInput
     out << @builder.text_field(field_name,
         options.merge(value: field_value, name: field_name, id: field_id, required: required))
     out << '  </div>'
-    out << '</div>' # row
 
+    # --- delete checkbox
+    if repeats == true
+      field_label = 'Crystallographic structure'
+      out << "  <div class='col-md-3'>"
+      out << destroy_widget(attribute_name, index, field_label, parent)
+      out << '  </div>'
+    end
+
+    out << '</div>' # last row
     out
   end
 end
