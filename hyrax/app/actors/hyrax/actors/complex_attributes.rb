b2610419d09e30da1a8db7719142d9618ae58d36
Kosuke Tanabe <nabeta@fastmail.fm>
2023-05-04 18:18:23 +0900

    update actor and indexer

--- hyrax/app/actors/hyrax/actors/complex_attributes.rb
+++ hyrax/app/actors/hyrax/actors/complex_attributes.rb
@@ -54,6 +54,7 @@ module Hyrax
           complex_computational_method
           complex_crystallographic_structure
           complex_event
+          complex_experimental_method
           complex_source
           complex_event_date
           complex_funding_reference
