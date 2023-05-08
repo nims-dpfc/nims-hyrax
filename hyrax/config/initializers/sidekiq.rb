e5603187850074481aa7635e92090d20681fa199
Kosuke Tanabe <TANABE.Kosuke@nims.go.jp>
2023-05-08 11:39:01 +0900

    fix setting log level

--- hyrax/config/initializers/sidekiq.rb
+++ hyrax/config/initializers/sidekiq.rb
@@ -3,10 +3,10 @@
 config = YAML.safe_load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 redis_config = config.merge(thread_safe: true)
 
-Sidekiq::Logging.logger.level = Logger::WARN if ENV['RAILS_ENV'] == 'production'
 
 Sidekiq.configure_server do |s|
   s.redis = redis_config
+  s.logger.level = Logger::WARN if ENV['RAILS_ENV'] == 'production'
 end
 
 Sidekiq.configure_client do |s|
