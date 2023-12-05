c5bf92e14a8415a99c7bdd14c23cb0bfeea78751
Kosuke Tanabe <nabeta@fastmail.fm>
2023-12-05 16:16:58 +0900

    fix property name

--- hyrax/app/models/dataset.rb
+++ hyrax/app/models/dataset.rb
@@ -82,7 +82,7 @@ class Dataset < ActiveFedora::Base
 
   property :complex_instrument, predicate: ::RDF::Vocab::NimsRdp.instrument, class_name: "ComplexInstrument"
 
-  property :complex_instrument_operator, predicate: ::RDF::Vocab::NimsRdp.instrument, class_name: "ComplexInstrumentOperator"
+  property :complex_instrument_operator, predicate: ::RDF::Vocab::NimsRdp.instrument_operator, class_name: "ComplexInstrumentOperator"
 
   property :origin_system_provenance, predicate: ::RDF::Vocab::NimsRdp['origin-system-provenance'], multiple: false do |index|
     index.as :stored_searchable
