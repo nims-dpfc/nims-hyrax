23cdb1763ab258e5b5b14f851f088db004d43fe9
Kosuke Tanabe <nabeta@fastmail.fm>
2024-02-26 12:27:45 +0900

    fix spec file

--- hyrax/spec/controllers/hyrax/users_controller_spec.rb
+++ hyrax/spec/controllers/hyrax/users_controller_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Hyrax::UsersController do
 
       it "should be redirected to SAMURAI" do
         get :show, params: { id: @user.user_identifier }
-        expect(response).to redirect_to "https://samurai.nims.go.jp/orcid/#{Hyrax::OrcidValidator.match(@user.orcid)}"
+        expect(response).to redirect_to "https://samurai.nims.go.jp/orcid/#{Hyrax::OrcidValidator.extract_bare_orcid(from: @user.orcid)}"
       end
     end
   end
